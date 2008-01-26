90e9e0542d1d7c4347e7e05d8c307a4b4b9e779a
kwan3218 <kwan3218@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2008-01-26 18:06:16 +0000

    Copied remotely

---
+++ tags/Original MG_Atlantis/Orbitersdk/Deluxe/meshres.h
@@ -0,0 +1,50 @@
+// ========================================================
+// Mesh resource file for ..\..\..\Meshes\Atlantis\Atlantis.msh
+// Generated with meshc on Sat Sep 23 02:41:10 2006
+
+// ========================================================
+
+// Number of mesh groups:
+#define NGRP 59
+
+// Number of materials:
+#define NMAT 4
+
+// Number of textures:
+#define NTEX 2
+
+// Named mesh groups:
+#define GRP_cargodooroutR 2
+#define GRP_cargodooroutL 3
+#define GRP_flapR 4
+#define GRP_flapL 5
+#define GRP_aileronR 6
+#define GRP_aileronL 7
+#define GRP_nosedoorL 15
+#define GRP_geardoorL 16
+#define GRP_geardoorR 17
+#define GRP_nosedoorR 18
+#define GRP_nosewheel 19
+#define GRP_nosegear 20
+#define GRP_wheelR 21
+#define GRP_gearR 22
+#define GRP_wheelL 23
+#define GRP_gearL 24
+#define GRP_startrackers 25
+#define GRP_rudderR 26
+#define GRP_rudderL 27
+#define GRP_cargodoorinR 31
+#define GRP_cargodoorinL 32
+#define GRP_KUband1 33
+#define GRP_KUband2 34
+#define GRP_Shoulder 35
+#define GRP_Humerus 36
+#define GRP_radii 37
+#define GRP_wrist 38
+#define GRP_endeffecter 39
+#define GRP_RMScamera 40
+#define GRP_RMScamera_pivot 41
+#define GRP_radiatorFL 50
+#define GRP_radiatorFR 51
+#define GRP_radiatorBR 52
+#define GRP_radiatorBL 53
