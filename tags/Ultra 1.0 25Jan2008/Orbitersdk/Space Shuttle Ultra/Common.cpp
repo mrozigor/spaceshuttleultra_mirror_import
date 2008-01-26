f214a398bec3b0825fbb1b6165f19ad54fc21d51
kwan3218 <kwan3218@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2008-01-26 18:48:43 +0000

    Copied remotely

---
+++ tags/Ultra 1.0 25Jan2008/Orbitersdk/Space Shuttle Ultra/Common.cpp
@@ -0,0 +1,84 @@
+// ==============================================================
+//                 ORBITER MODULE: Atlantis
+//                  Part of the ORBITER SDK
+//          Copyright (C) 2001-2003 Martin Schweiger
+//                   All rights reserved
+//
+// Common.cpp
+// Utility functions common to multiple Atlantis-related modules
+// ==============================================================
+
+#include "Atlantis.h"
+
+#ifdef _DEBUG
+// D. Beachy: GROW THE STACK HERE SO WE CAN USE BOUNDSCHECKER FOR DEBUGGING
+// We need this is because BoundsChecker (for this object) grows the stack more than 1 full page (4K) at once
+// and then touches data beyond the initial 4K, skipping over the guard page that Windows places below the stack to grow it automatically.
+// Therefore we will grow the stack manually in one-page increments here.
+// This is only necessary for BoundsChecker debugging.
+
+int GrowStack()
+{
+    // NOTE: this requires that orbiter.exe has its 'Size of Stack Reserve' PE header parameter set to 4 MB
+    int pageCount = 256;    // 256 4K pages = reserve 1 MB of stack
+    DWORD dwStackDelta = 0; // total # of stack bytes used
+    for (int i=0; i < pageCount; i++)
+    {
+        dwStackDelta += 4096;
+        __asm
+        {
+            sub     esp, 4092;  // 1 page - 4 bytes
+            push    0xFEEDFEED  // touch the page
+        }
+    }
+
+    // now pop the stack we touched
+    __asm
+    {
+        mov     eax, [dwStackDelta] // size in bytes
+        add     esp, eax
+    }
+
+    return 0;
+}
+
+// invoke GrowStack early before the next lines are called (otherwise BoundsChecker will crash)
+int growStack=GrowStack();
+#endif
+
+double SRB_Seq[SRB_nt]  = {-SRB_STABILISATION_TIME,-0.001,0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,105,110,115,120,125,130,160};
+double SRB_Thrust[SRB_nt] = { 0,0,0.933,0.957,0.986,0.992,1.000,0.947,0.889,0.842,0.801,0.771,0.737,0.748,0.775,0.798,
+                                0.819,0.829,0.811,0.755,0.715,0.686,0.639,0.582,0.550,0.257,0.107,0.023,0.010,0.000};
+double SRB_Prop[SRB_nt]   = { 1,1,1.000,0.951,0.900,0.848,0.796,0.743,0.693,0.646,0.602,0.559,0.519,0.480,0.440,0.400,
+                                0.357,0.314,0.270,0.228,0.188,0.150,0.114,0.080,0.049,0.020,0.007,0.002,0.001,0.000};
+
+
+//PARTICLESTREAMSPEC srb_contrail = {
+//	0, 12.0, 3, 150.0, 0.4, 8.0, 4, 3.0, PARTICLESTREAMSPEC::DIFFUSE,
+//	PARTICLESTREAMSPEC::LVL_PSQRT, 0, 0.5,
+//	PARTICLESTREAMSPEC::ATM_PLOG, 1e-6, 0.1
+//};
+PARTICLESTREAMSPEC srb_contrail = {
+	0, 12.0, 3, 150.0, 0.2, 8.0, 4, 3.0, PARTICLESTREAMSPEC::DIFFUSE,
+	PARTICLESTREAMSPEC::LVL_PSQRT, 0, 0.5,
+	PARTICLESTREAMSPEC::ATM_PLOG, 1e-6, 0.1
+};
+PARTICLESTREAMSPEC srb_exhaust = {
+	0, 3.0, 40, 250.0, 0.04, 0.7, 10, 3.0, PARTICLESTREAMSPEC::EMISSIVE,
+	PARTICLESTREAMSPEC::LVL_FLAT, 1, 1,
+	PARTICLESTREAMSPEC::ATM_FLAT, 1, 1
+};
+
+// time-dependent calculation of SRB thrust and remaining propellant
+void GetSRB_State (double met, double &thrust_level, double &prop_level)
+{
+	int i;
+	for (i = SRB_nt-2; i >= 0; i--)
+		if (met > SRB_Seq[i]) break;
+    double SRB_ThrSCL=(SRB_Thrust[i+1]-SRB_Thrust[i])/(SRB_Seq[i+1]-SRB_Seq[i]);
+    double SRB_PrpSCL=(SRB_Prop[i+1]-SRB_Prop[i])/(SRB_Seq[i+1]-SRB_Seq[i]);
+	thrust_level = SRB_ThrSCL * (met-SRB_Seq[i]) + SRB_Thrust[i];
+	if(met>0.0) thrust_level+=0.01*oapiRand() - 0.01*oapiRand();
+	prop_level = SRB_PrpSCL * (met-SRB_Seq[i]) + SRB_Prop[i];
+}
+
---
+++ tags/Ultra 1.0 25Jan2008/Orbitersdk/Space Shuttle Ultra/meshres.h
@@ -0,0 +1,75 @@
+// ========================================================
+// Mesh resource file for c:\Orbiter-Trunk\Meshes\Atlantis\Atlantis.msh
+// Generated with meshc on Sun Jan 28 22:10:55 2007
+
+// ========================================================
+
+// Number of mesh groups:
+#define NGRP 50
+
+// Number of materials:
+#define NMAT 4
+
+// Number of textures:
+#define NTEX 2
+
+// Named mesh groups:
+#define GRP_bodyflap 1
+#define GRP_cargodooroutR 2
+#define GRP_cargodooroutL 3
+#define GRP_flapR 4
+#define GRP_flapL 5
+#define GRP_aileronR 6
+#define GRP_aileronL 7
+#define GRP_etumbdoorR 8
+#define GRP_etumbdoorL 9
+#define GRP_nosedoorL 15
+#define GRP_geardoorL 16
+#define GRP_geardoorR 17
+#define GRP_nosedoorR 18
+#define GRP_nosewheel 19
+#define GRP_nosegear 20
+#define GRP_wheelR 21
+#define GRP_gearR 22
+#define GRP_wheelL 23
+#define GRP_gearL 24
+#define GRP_startrackers 25
+#define GRP_rudderR 26
+#define GRP_rudderL 27
+#define GRP_cargodoorinR 31
+#define GRP_cargodoorinL 32
+#define GRP_KUband1 33
+#define GRP_KUband2 34
+#define GRP_camerapivotFL 35
+#define GRP_cameraFL 36
+#define GRP_cameraBL 37
+#define GRP_camerapivotBL 38
+#define GRP_cameraFR 39
+#define GRP_camerapivotFR 40
+#define GRP_camerapivotBR 41
+#define GRP_cameraBR 42
+#define GRP_radiatorFL 43
+#define GRP_radiatorFR 44
+#define GRP_radiatorBR 45
+#define GRP_radiatorBL 46
+/*#define GRP_Shoulder 35
+#define GRP_Humerus 36
+#define GRP_radii 37
+#define GRP_wrist 38
+#define GRP_endeffecter 39
+#define GRP_RMScamera 40
+#define GRP_RMScamera_pivot 41
+#define GRP_camerapivotFL 42
+#define GRP_cameraFL 43
+#define GRP_cameraBL 44
+#define GRP_camerapivotBL 45
+#define GRP_cameraFR 46
+#define GRP_camerapivotFR 47
+#define GRP_camerapivotBR 48
+#define GRP_cameraBR 49
+#define GRP_radiatorFL 50
+#define GRP_radiatorFR 51
+#define GRP_radiatorBR 52
+#define GRP_radiatorBL 53
+#define GRP_portMPMs 57
+#define GRP_shouldermount 58*/
