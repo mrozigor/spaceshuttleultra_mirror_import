5aa10632c98bfb3aa93907cd7406f29737b3d0e6
kwan3218 <kwan3218@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2008-01-26 17:54:16 +0000

    Original MG_Atlantis as extracted from orbiter060929_sdk.zip . Original parent for Deluxe and Ultra.

---
+++ trunk/Orbitersdk/MG_Atlantis/meshres_vc_hi.h
@@ -0,0 +1,40 @@
+// ========================================================
+// Mesh resource file for ..\..\..\Meshes\Atlantis\AtlantisVC.msh
+// Generated with meshc on Tue Aug 29 04:24:27 2006
+
+// ========================================================
+
+// Number of mesh groups:
+#define NGRP_VC 383
+
+// Number of materials:
+#define NMAT_VC 20
+
+// Number of textures:
+#define NTEX_VC 15
+
+// Named mesh groups:
+#define GRP_CDR1_VC 33
+#define GRP_CDR2_VC 34
+#define GRP_PLT1_VC 35
+#define GRP_PLT2_VC 36
+#define GRP_MFD1_VC 37
+#define GRP_MFD2_VC 38
+#define GRP_MFD3_VC 39
+#define GRP_MFD4_VC 40
+#define GRP_MFD5_VC 41
+#define GRP_MFD_aft_VC 42
+#define GRP_VirtualHUD_VC 59
+#define GRP_SwitchR13L_1_VC 60
+#define GRP_SwitchR13L_2_VC 61
+#define GRP_SwitchR13L_3_VC 62
+#define GRP_SwitchR13L_4_VC 63
+#define GRP_SwitchR13L_5_VC 64
+#define GRP_SwitchR13L_6_VC 65
+#define GRP_SwitchR13L_7_VC 66
+#define GRP_SwitchR13L_8_VC 67
+#define GRP_SwitchR13L_9_VC 68
+#define GRP_SwitchR13L_10_VC 69
+#define GRP_SwitchR13L_11_VC 70
+#define GRP_SwitchR13L_12_VC 71
+#define GRP_SwitchR13L_13_VC 72
