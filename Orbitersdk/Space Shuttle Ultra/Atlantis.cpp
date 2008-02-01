c694713a32d232e4d67526df5dbb2d0c05b3889f
orbiter_fan <orbiter_fan@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2008-02-01 14:53:42 +0000

    Contains new parameters for SRB exhaust and contrail. Also includes fixed SRB exhaust offsets so that the exhausts line up correctly with booster nozzles.

--- Orbitersdk/Space Shuttle Ultra/Atlantis.cpp
+++ Orbitersdk/Space Shuttle Ultra/Atlantis.cpp
@@ -633,10 +633,10 @@ void Atlantis::SetLaunchConfiguration (void)
   srb_exhaust.tex = oapiRegisterParticleTexture ("Contrail2");
   srb_contrail.tex = oapiRegisterParticleTexture ("Contrail4");
   for (i = 0; i < 2; i++) AddExhaust (th_srb[i], 16.0, 2.0, tex);
-  AddExhaustStream (th_srb[0], OFS_LAUNCH_RIGHTSRB+_V(0,0,/*-30*/-50), &srb_contrail);
-  AddExhaustStream (th_srb[1], OFS_LAUNCH_LEFTSRB+_V(0,0,/*-30*/-50), &srb_contrail);
-  AddExhaustStream (th_srb[0], OFS_LAUNCH_RIGHTSRB+_V(0,0,-25), &srb_exhaust);
-  AddExhaustStream (th_srb[1], OFS_LAUNCH_LEFTSRB+_V(0,0,-25), &srb_exhaust);
+  AddExhaustStream (th_srb[0], OFS_LAUNCH_RIGHTSRB+_V(0,1,/*-30*/-135), &srb_contrail); //DaveS edit: Modified Zofs from 50 to 135 to take in account longer exhaust
+  AddExhaustStream (th_srb[1], OFS_LAUNCH_LEFTSRB+_V(0,1,/*-30*/-135), &srb_contrail); //DaveS edit: Modified Zofs from 50 to 135 to take in account longer exhaust
+  AddExhaustStream (th_srb[0], OFS_LAUNCH_RIGHTSRB+_V(-0.8,1,-23), &srb_exhaust); //DaveS edit: Modified Zofs from -25 to -23 to better line up the exhaust PSTREAM with SRB nozzle, also corrected xofs and yofs to fix alignment issue
+  AddExhaustStream (th_srb[1], OFS_LAUNCH_LEFTSRB+_V(0.8,1,-23), &srb_exhaust); //DaveS edit: Modified Zofs from -25 to -23 to better line up the exhaust PSTREAM with SRB nozzle, also corrected xofs and yofs to fix alignment issue
 
   //OMS
   VECTOR3 OMS_POS=OFS_LAUNCH_ORBITER+_V(0,3.7,-13.8);
