a5b39c5e86326c4e416f3cc43b94010c7232b838
Urwumpe <Urwumpe@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2008-02-03 02:00:13 +0000

    Fixed Booster separation sequence and added BSM motor exhaust. Also included is slag particle effect when SRB thrust drops.
    References to OMS code are existing, but excluded from compilation

--- Orbitersdk/Space Shuttle Ultra/Atlantis_SRB.cpp
+++ Orbitersdk/Space Shuttle Ultra/Atlantis_SRB.cpp
@@ -25,7 +25,11 @@ Atlantis_SRB::Atlantis_SRB (OBJHANDLE hObj)
 : VESSEL2(hObj)
 {
 	// preload mesh
-	hSRBMesh = oapiLoadMeshGlobal ("Atlantis_srb");
+	hSRBMesh_Left = oapiLoadMeshGlobal ("Shuttle_lsrb");
+	hSRBMesh_Right = oapiLoadMeshGlobal ("Shuttle_rsrb");
+	slag1 = 1.0;
+	slag2 = 1.0;
+	slag3 = 1.0;
 }
 
 // reconstruct liftoff time from fuel level
@@ -51,14 +55,28 @@ void Atlantis_SRB::clbkSetClassCaps (FILEHANDLE cfg)
 {
 	bool Left;
 	oapiReadItem_bool(cfg, "Left", Left);
-	if(Left) hSRBMesh = oapiLoadMeshGlobal ("Shuttle_lsrb");
-	else hSRBMesh = oapiLoadMeshGlobal ("Shuttle_rsrb");
-	extern PARTICLESTREAMSPEC srb_contrail, srb_exhaust;
+
+
+	if(Left) 
+	{
+		hSRBMesh = hSRBMesh_Left;
+	//	oapiWriteLog("Create Left SRB\n");
+	}
+	else 
+	{
+		hSRBMesh = hSRBMesh_Right;
+	//	oapiWriteLog("Create Right SRB\n");
+	}
+
+	extern PARTICLESTREAMSPEC srb_contrail, srb_exhaust, srb_slag1, srb_slag2, srb_slag3;
+	extern PARTICLESTREAMSPEC bsm_exhaust, bsm_contrail, bsm_residual;
 	PARTICLESTREAMSPEC srb_bolt = {
 		0, 8.0, 20, 0.0, 0.1, 0.3, 16, 3.0, PARTICLESTREAMSPEC::EMISSIVE,
 		PARTICLESTREAMSPEC::LVL_LIN, 0, 1,
 		PARTICLESTREAMSPEC::ATM_FLAT, 1, 1
 	};
+
+	
 
 	SetEnableFocus (false);
 	// SRB cannot receive input focus
@@ -78,6 +96,10 @@ void Atlantis_SRB::clbkSetClassCaps (FILEHANDLE cfg)
 	// ************************* propellant specs **********************************
 
 	ph_main = CreatePropellantResource (SRB_MAX_PROPELLANT_MASS);
+
+	//75 pounds of fuel unburned
+
+	phBSM = CreatePropellantResource(BSM_MAX_PROPELLANT_MASS * 8.0);
 	// Note that the SRB instance is only created after separation from
 	// the main assembly, so the actual fuel mass will always be
 	// much smaller
@@ -87,19 +109,41 @@ void Atlantis_SRB::clbkSetClassCaps (FILEHANDLE cfg)
 	// main engine
 	th_main = CreateThruster (_V(0,0,-21), _V(0,0,1), SRB_THRUST, ph_main, SRB_ISP0, SRB_ISP1);
 	SURFHANDLE tex = oapiRegisterExhaustTexture ("Exhaust2");
-	srb_exhaust.tex = oapiRegisterParticleTexture ("Contrail2");
+	bsm_exhaust.tex = srb_exhaust.tex = oapiRegisterParticleTexture ("Contrail2");
+	
 	AddExhaust (th_main, 16.0, 2.0, tex);
 	AddExhaustStream (th_main, _V(0,0,-30), &srb_contrail);
 	AddExhaustStream (th_main, _V(0,0,-25), &srb_exhaust);
 
 	// separation bolts
 	th_bolt = CreateThruster (_V(0,0,1.3), _V(1,0,0), 3e6, ph_main, 1e7);
+
+	//BSM
+	if(Left)
+	{
+		thBSM[0] = CreateThruster(_V(0.752, 2.06, -20.5), _V(-0.22, -0.604, 0.766), 4*BSM_THRUST0, phBSM, BSM_ISP0);
+		thBSM[1] = CreateThruster(_V(0.445, 1.22, 21), _V(-0.22, -0.604, -0.766), 4*BSM_THRUST0, phBSM, BSM_ISP0);
+	} else 
+	{
+		thBSM[0] = CreateThruster(_V(-0.752, 2.06, -20.5), _V(0.22, -0.604, 0.766), 4*BSM_THRUST0, phBSM, BSM_ISP0);
+		thBSM[1] = CreateThruster(_V(-0.445, 1.22, 21), _V(0.22, -0.604, -0.766), 4*BSM_THRUST0, phBSM, BSM_ISP0);
+	}
 	// for simplicity, the separation bolts directly use SRB propellant. We give
 	// them an insanely high ISP to avoid significant propellant drainage
 
 	AddExhaust (th_bolt, 0.7, 0.1, _V(-2.1,0,-8), _V(-1,0,0));
 	AddExhaust (th_bolt, 0.7, 0.1, _V(-2.1,0,11), _V(-1,0,0));
+
+	AddExhaustStream (thBSM[0], &bsm_exhaust);
+	AddExhaustStream (thBSM[1], &bsm_exhaust);
+	AddExhaustStream (thBSM[0], &bsm_contrail);
+	AddExhaustStream (thBSM[1], &bsm_contrail);
 	AddExhaustStream (th_bolt, _V(-2.1,0,0), &srb_bolt);
+
+	//Add slag effect streams
+	AddParticleStream(&srb_slag1, _V(0,0,-22), SLAG_DIR, &slag1);
+	AddParticleStream(&srb_slag2, _V(0,0,-22), SLAG_DIR, &slag2);
+	AddParticleStream(&srb_slag3, _V(0,0,-22), SLAG_DIR, &slag3);
 
 	// ************************ visual parameters **********************************
 
@@ -123,6 +167,18 @@ void Atlantis_SRB::clbkPostStep (double simt, double simdt, double mjd)
 	//sprintf (oapiDebugString(), "SRB mass = %f", GetMass());
 	if (bMainEngine) {
 		double met = simt-t0;
+		if (met >= NO_SLAG_TIME)
+		{
+			slag1 = 0.0;
+			slag2 = 0.0;
+			slag3 = 0.0;
+		} else 
+		{
+			slag1 = 1.0 - met/NO_SLAG_TIME;
+			slag2 = 1.0 - met/NO_SLAG_TIME;
+			slag3 = 1.0 - met/NO_SLAG_TIME;
+		}
+
 		if (met >= SRB_CUTOUT_TIME) {
 			SetThrusterLevel (th_main, 0);
 			bMainEngine = false;
@@ -137,15 +193,27 @@ void Atlantis_SRB::clbkPostStep (double simt, double simdt, double mjd)
 		if (bSeparationEngine) {
 			static double bolt_t = 0.5;
 			double srb_dt = simt - srb_separation_time;
+
+			if(srb_dt < 1.2)
+			{
+				SetThrusterLevel (thBSM[0], 1.0-srb_dt * 0.583);
+				SetThrusterLevel (thBSM[1], 1.0-srb_dt * 0.583);
+			} else if (srb_dt < 30.0) {
+				//show residual clouds
+			}
+			/*
 			if (srb_dt > bolt_t) {
 				DelThruster (th_bolt);
 				bSeparationEngine = false;
 			} else {
 				SetThrusterLevel (th_bolt, sqrt (1.0-srb_dt/bolt_t));
+				
 			}
+			*/
 		}
 	}
-	if (GetAltitude() < 0.0) oapiDeleteVessel (GetHandle());
+	//increased altitude for self delete
+	if (GetAltitude() < 250.0) oapiDeleteVessel (GetHandle());
 }
 
 // ==============================================================
