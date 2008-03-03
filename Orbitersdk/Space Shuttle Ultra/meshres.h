4ec4446f72c3b3c54d74cb55a7f084d619d7ab41
orbiter_fan <orbiter_fan@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2008-03-03 18:06:19 +0000

    New meshres.h file for the main orbiter mesh, tanking in account the new separate startracker doors, seperate elevons and flipper doors and air data probes.

--- Orbitersdk/Space Shuttle Ultra/meshres.h
+++ Orbitersdk/Space Shuttle Ultra/meshres.h
@@ -1,66 +1,74 @@
 // ========================================================
-// Mesh resource file for ..\..\Meshes\Atlantis\Atlantis.msh
-// Generated with meshc on Sun Feb 17 01:46:06 2008
+// Mesh resource file for Atlantis.msh
+// Generated with meshc on Mon Mar 03 19:04:41 2008
 
 // ========================================================
 
 // Number of mesh groups:
-#define NGRP 50
+#define NGRP 58
 
 // Number of materials:
-#define NMAT 3
+#define NMAT 5
 
 // Number of textures:
 #define NTEX 2
 
 // Named mesh groups:
-#define GRP_FUSELAGE 0
-#define GRP_BODYFLAP 1
-#define GRP_CARGODOOROUTR 2
-#define GRP_CARGODOOROUTL 3
-#define GRP_FLAPR 4
-#define GRP_FLAPL 5
-#define GRP_AILERONR 6
-#define GRP_AILERONL 7
+#define GRP_Fuselage 0
+#define GRP_bodyflap 1
+#define GRP_cargodooroutR 2
+#define GRP_cargodooroutL 3
+#define GRP_flapR 4
+#define GRP_flapL 5
+#define GRP_aileronR 6
+#define GRP_aileronL 7
 #define GRP_ETUMBDOORR 8
 #define GRP_ETUMBDOORL 9
-#define GRP_OMSL 10
-#define GRP_OMSR 11
-#define GRP_SSMER 12
-#define GRP_SSMEL 13
-#define GRP_SSMET 14
-#define GRP_NOSEDOORL 15
-#define GRP_GEARDOORL 16
-#define GRP_GEARDOORR 17
-#define GRP_NOSEDOORR 18
-#define GRP_NOSEWHEEL 19
-#define GRP_NOSEGEAR 20
-#define GRP_WHEELR 21
-#define GRP_GEARR 22
-#define GRP_WHEELL 23
-#define GRP_GEARL 24
-#define GRP_STARTRACKERS 25
-#define GRP_RUDDERR 26
-#define GRP_RUDDERL 27
-#define GRP_PROBEL 28
-#define GRP_PROBER 29
+#define GRP_omsL 10
+#define GRP_omsR 11
+#define GRP_ssmeR 12
+#define GRP_ssmeL 13
+#define GRP_ssmeT 14
+#define GRP_nosedoorL 15
+#define GRP_geardoorL 16
+#define GRP_geardoorR 17
+#define GRP_nosedoorR 18
+#define GRP_nosewheel 19
+#define GRP_nosegear 20
+#define GRP_wheelR 21
+#define GRP_gearR 22
+#define GRP_wheelL 23
+#define GRP_gearL 24
+#define GRP_trackbox 25
+#define GRP_rudderR 26
+#define GRP_rudderL 27
+#define GRP_probeL 28
+#define GRP_probeR 29
 #define GRP_CARGOBAY 30
-#define GRP_CARGODOORINR 31
-#define GRP_CARGODOORINL 32
-#define GRP_KUBAND1 33
-#define GRP_KUBAND2 34
-#define GRP_CAMERAPIVOTFL 35
-#define GRP_CAMERAFL 36
-#define GRP_CAMERABL 37
-#define GRP_CAMERAPIVOTBL 38
-#define GRP_CAMERAFR 39
-#define GRP_CAMERAPIVOTFR 40
-#define GRP_CAMERAPIVOTBR 41
-#define GRP_CAMERABR 42
-#define GRP_RADIATORFL 43
-#define GRP_RADIATORFR 44
-#define GRP_RADIATORBR 45
-#define GRP_RADIATORBL 46
-#define GRP_HUDL 47
-#define GRP_HUDR 48
-#define GRP_WINDOWS 49
+#define GRP_cargodoorinR 31
+#define GRP_cargodoorinL 32
+#define GRP_KUband1 33
+#define GRP_KUband2 34
+#define GRP_camerapivotFL 35
+#define GRP_cameraFL 36
+#define GRP_cameraBL 37
+#define GRP_camerapivotBL 38
+#define GRP_cameraFR 39
+#define GRP_camerapivotFR 40
+#define GRP_camerapivotBR 41
+#define GRP_cameraBR 42
+#define GRP_radiatorFL 43
+#define GRP_radiatorFR 44
+#define GRP_radiatorBR 45
+#define GRP_radiatorBL 46
+#define GRP_startrackers 47
+#define GRP_trackerplates 48
+#define GRP_Z-startrackdoor 49
+#define GRP_Y-startrackdoor 50
+#define GRP_LeftFlapperIN 51
+#define GRP_LeftFlapperOUT 52
+#define GRP_RightFlapperIN 53
+#define GRP_RightFlapperOUT 54
+#define GRP_hudL 55
+#define GRP_hudR 56
+#define GRP_windows 57
