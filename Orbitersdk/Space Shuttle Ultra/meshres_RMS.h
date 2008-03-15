493e6c9821a5aff6ed2390bad2f38a206981b231
orbiter_fan <orbiter_fan@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2008-03-15 14:11:05 +0000

    New RMS mesh with seperate elbow camera. Also fixes the new blocked Canda logos.

--- Orbitersdk/Space Shuttle Ultra/meshres_RMS.h
+++ Orbitersdk/Space Shuttle Ultra/meshres_RMS.h
@@ -1,3 +1,9 @@
+// ========================================================
+// Mesh resource file for RMS.msh
+// Generated with meshc on Sat Mar 15 15:07:09 2008
+
+// ========================================================
+
 // Number of mesh groups:
 #define NGRP_RMS 8
 
@@ -13,6 +19,9 @@
 #define GRP_Shoulder_RMS 2
 #define GRP_Humerus_RMS 3
 #define GRP_radii_RMS 4
-#define GRP_wristpitch_RMS 5
-#define GRP_wristyaw_RMS 6
-#define GRP_endeffecter_RMS 7
+#define GRP_elbowcam 5
+#define GRP_camswivel 6
+#define GRP_cambase 7
+#define GRP_wristpitch_RMS 8
+#define GRP_wristyaw_RMS 9
+#define GRP_endeffecter_RMS 10
