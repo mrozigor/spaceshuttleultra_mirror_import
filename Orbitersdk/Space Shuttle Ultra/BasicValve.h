78037a39ac5252bbb8dc75507dbbfca5b5e7b3d4
GLS_SSU <GLS_SSU@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2008-07-29 23:04:41 +0000

    Vehicle Data Table (VDT) data fields list and some manually-made Doxygen comments (hope they work!)

--- Orbitersdk/Space Shuttle Ultra/BasicValve.h
+++ Orbitersdk/Space Shuttle Ultra/BasicValve.h
@@ -12,22 +12,71 @@ private:
 	double mpos;
 	double mrate;
 public:
+	/**
+	 * Opens valve using specific rate
+  	 * @param rate rate
+	 */
+	bool Open( double rate );
 
-	bool Open( double );
-	bool Open( void );// use max rate
-	bool Close( double );
-	bool Close( void );// use max rate
-	bool Move( double );
-	bool Move( double, double );
+	/**
+	 * Opens valve using maximum rate
+	 */
+	bool Open( void );
+
+	/**
+	 * Closes valve using specific rate
+  	 * @param rate rate
+	 */
+	bool Close( double rate );
+
+	/**
+	 * Closes valve using maximum rate
+	 */
+	bool Close( void );
+
+	/**
+	 * Moves valve to target position using specific rate
+ 	 * @param tpos target position
+  	 * @param rate rate
+	 */
+	bool Move( double tpos, double rate );
+
+	/**
+	 * Moves valve to target position using maximum rate
+ 	 * @param tpos target position
+	 */
+	bool Move( double tpos );
+
+	/**
+	 * Returns valve position
+	 * @return valve position (range: 0 - closed, 1 - open)
+	 */
 	double GetPos( void );
-	void tmestp( double );
-	void _backdoor( double );
 
-	BasicValve( double, double );
+	/**
+	 * Updates valve position (call from time step functions)
+	 * @param dtme sim dt
+	 */
+	void tmestp( double dtme );
+
+	/**
+	 * Use from .scn loading function to set valve position
+	 * @param ipos valve position
+	 */
+	void _backdoor( double ipos );
+
+	/**
+	 * Create a new valve
+	 * @param initpos initial valve position
+	 * @param imaxrate maximum valve motion rate
+	 */
+	BasicValve( double initpos, double imaxrate );
 	~BasicValve( void );
 };
 
+
 #endif// _g_BasicValve_H_
+
 
 /*
 // 1->open, 0->closed
