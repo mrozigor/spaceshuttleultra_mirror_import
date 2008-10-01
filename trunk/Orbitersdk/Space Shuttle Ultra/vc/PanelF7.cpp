70926f3351eb05691a87e7c7e7fffedbe3639ba8
siamesecat <siamesecat@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2008-10-01 22:52:10 +0000

    Added timer

--- trunk/Orbitersdk/Space Shuttle Ultra/vc/PanelF7.cpp
+++ trunk/Orbitersdk/Space Shuttle Ultra/vc/PanelF7.cpp
@@ -1,7 +1,10 @@
 #include "PanelF7.h"
 #include "../Atlantis.h"
 #include "../meshres_vc.h"
+#include "../meshres_vc_additions.h"
 #include "MDU.h"
+
+extern GDIParams g_Param;
 
 namespace vc {
 
@@ -14,6 +17,8 @@ namespace vc {
 		Add(pMFD1 = new MDU(_sts, "MFD1", MDUID_MFD1, true));
 		Add(pMFD2 = new MDU(_sts, "MFD2", MDUID_MFD2, true));
 		
+		sTimerMinutes=0;
+		sTimerSeconds=0;
 	}
 
 	PanelF7::~PanelF7()
@@ -42,9 +47,50 @@ namespace vc {
 		BasicPanel::RegisterVC();
 		VECTOR3 ofs = STS()->orbiter_ofs;
 		oapiVCRegisterArea(AID_F7, PANEL_REDRAW_NEVER, PANEL_MOUSE_LBDOWN | PANEL_MOUSE_LBUP | PANEL_MOUSE_LBPRESSED);
+		//timer
+		SURFHANDLE digit_tex = oapiGetTextureHandle(STS()->hOrbiterVCMesh, TEX_DIGITS_VC);
+		oapiVCRegisterArea(AID_F7_EVTTMR1, _R(0, 320, 256, 384), PANEL_REDRAW_USER, PANEL_MOUSE_IGNORE, PANEL_MAP_CURRENT, digit_tex);	
+		
 		oapiVCSetAreaClickmode_Quadrilateral (AID_F7, 
 			_V(-0.389, 2.407, 14.804)+ofs, _V(0.385, 2.407, 14.805)+ofs,
 			_V(-0.389, 1.962, 14.678)+ofs, _V(0.385, 1.962, 14.678) + ofs);
 	}
+	
+	void PanelF7::OnPostStep(double fSimT, double fDeltaT, double fMJD)
+	{		
+		//Check forward event timer for changes and update clock if needed
+		if(STS()->pMTU->GetEventTimerMin(dps::TIMER_FORWARD) != sTimerMinutes || 
+			STS()->pMTU->GetEventTimerSec(dps::TIMER_FORWARD) != sTimerSeconds)
+		{
+			oapiWriteLog("PanelF7::OnPostStep called");
+
+			sTimerMinutes = STS()->pMTU->GetEventTimerMin(dps::TIMER_FORWARD);
+			sTimerSeconds = STS()->pMTU->GetEventTimerSec(dps::TIMER_FORWARD);
+			oapiVCTriggerRedrawArea(-1, AID_F7_EVTTMR1);
+		}
+	}
+
+	bool PanelF7::OnVCRedrawEvent(int id, int _event, SURFHANDLE surf)
+	{
+		const int NUMX[10] = {64, 0, 64, 128, 192, 256, 320, 384, 448, 0};
+		const int NUMY[10] = {384, 448, 448, 448, 448, 448, 448, 448, 448, 384};
+		int i;
+		int digit[4];
 		
+		if(id == AID_F7_EVTTMR1)
+		{
+			oapiWriteLog("PanelF7::OnVCRedrawEvent called");
+
+			digit[0] = sTimerMinutes / 10;
+			digit[1] = sTimerMinutes % 10;
+			digit[2] = sTimerSeconds / 10;
+			digit[3] = sTimerSeconds % 10;
+
+			for(i = 0; i<4; i++)
+			{
+				oapiBlt(surf, g_Param.digits_7seg, i*64, 0, NUMX[digit[i]], NUMY[digit[i]], 64, 64);
+			}
+		}
+		return true;
+	}
 };
