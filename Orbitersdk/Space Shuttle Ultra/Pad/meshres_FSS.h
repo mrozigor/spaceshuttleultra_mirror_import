4fd4f720be420410546b6d10034adb89304efed5
siamesecat <siamesecat@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2008-11-01 16:19:03 +0000

    Launch pad animations

---
+++ Orbitersdk/Space Shuttle Ultra/Pad/meshres_FSS.h
@@ -0,0 +1,57 @@
+// ========================================================
+// Mesh resource file for ..\..\Meshes\SSU\LC39A_FSS.msh
+// Generated with meshc on Thu Oct 30 19:57:19 2008
+
+// ========================================================
+
+// Number of mesh groups:
+#define NGRP 41
+
+// Number of materials:
+#define NMAT 3
+
+// Number of textures:
+#define NTEX 1
+
+// Named mesh groups:
+#define GRP_Orbiter_Access_Arm 0
+#define GRP_White_Room_Mat 1
+#define GRP_White_Room 2
+#define GRP_Cylinder324 3
+#define GRP_GUCP 4
+#define GRP_Box02 5
+#define GRP_Box03 6
+#define GRP_Box04 7
+#define GRP_Box06 8
+#define GRP_Box07 9
+#define GRP_Box08 10
+#define GRP_Box09 11
+#define GRP_Box10 12
+#define GRP_Box11 13
+#define GRP_Box12 14
+#define GRP_Box13 15
+#define GRP_Box14 16
+#define GRP_Box15 17
+#define GRP_Box16 18
+#define GRP_Box17 19
+#define GRP_Box21 20
+#define GRP_Box22 21
+#define GRP_MAPU 22
+#define GRP_Cylinder327 23
+#define GRP_Cylinder328 24
+#define GRP_Cylinder334 25
+#define GRP_FSS_WPS_Z_bracket 26
+#define GRP_GOX_Cylinder06 27
+#define GRP_GOX_Cylinder342 28
+#define GRP_GOX_vent_arm_truss 29
+#define GRP_Inner_FSS_WPS_panel_track 30
+#define GRP_Inner_WPS_panel 31
+#define GRP_Lightning_mast 32
+#define GRP_Line01 33
+#define GRP_Main_FSS 34
+#define GRP_North_GN2_vent_pipe 35
+#define GRP_Intertank 36
+#define GRP_Outer_WPS_panel 37
+#define GRP_South_GN2_vent_pipe 38
+#define GRP_Vent_hood 39
+#define GRP_Y_FSS_WPS_struts 40
