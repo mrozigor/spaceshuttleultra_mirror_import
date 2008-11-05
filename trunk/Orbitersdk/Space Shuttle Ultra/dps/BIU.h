b3e2136c3baf08a7336d96ba009b4a3f976e76d5
Urwumpe <Urwumpe@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2008-11-05 20:26:51 +0000

    Allowed giving BIUs labels and assign them to subsystems, which can be used for feedback.

--- trunk/Orbitersdk/Space Shuttle Ultra/dps/BIU.h
+++ trunk/Orbitersdk/Space Shuttle Ultra/dps/BIU.h
@@ -1,10 +1,17 @@
 #pragma once
 
 #include "dps_defs.h"
+#include <string>
+
+class AtlantisSubsystem;
 
 namespace dps {
 
+	using namespace std;
+
 	class BIU {
+		AtlantisSubsystem* pParent;
+		string label;
 		unsigned short usReceiveBuffer[1024];
 		unsigned short usTransmitBuffer[1024];
 		ShuttleBus* pBus;
@@ -15,6 +22,13 @@ namespace dps {
 		BIU();
 		BIU(unsigned short usTerminalID, bool bIsMaster = false);
 		virtual ~BIU();
+		void Label(const string& _label);
+		const string& GetLabel() const;
+		void SetParent(AtlantisSubsystem* pSubsys);
+		AtlantisSubsystem* GetParent() const;
+		void SetTerminalID(unsigned short TermID);
+		void Init(AtlantisSubsystem* pSubsys, const string& _label, 
+			unsigned short usTermID, bool bIsMaster);
 		void ConnectTo(ShuttleBus* pBus);
 		void MakeBusMaster();
 		void Listen(bool fEnable = true);
