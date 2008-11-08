21dfe9ff985a3e6c6f4d70e556d3252440f13683
Urwumpe <Urwumpe@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2008-11-08 22:50:13 +0000

    Added the remaining switches on the APDS control panel.

--- Orbitersdk/Space Shuttle Ultra/vc/PanelA7A8ODS.cpp
+++ Orbitersdk/Space Shuttle Ultra/vc/PanelA7A8ODS.cpp
@@ -25,19 +25,46 @@ namespace vc {
 		Add(pARLKTNLFanB		= new StdSwitch2(_sts, "ARLK/TNL FAN B"));
 		Add(pLightsCLVestPORT	= new StdSwitch2(_sts, "LIGHTS C/L VESTIBULE PORT"));
 		Add(pLightsCLVestSTBD	= new StdSwitch2(_sts, "LIGHTS C/L VESTIBULE STBD"));
+
+
 		Add(pControlPanelPowerA = new StdSwitch2(_sts, "CNTL PNL PWR A"));
 		Add(pControlPanelPowerB = new StdSwitch2(_sts, "CNTL PNL PWR B"));
 		Add(pControlPanelPowerC = new StdSwitch2(_sts, "CNTL PNL PWR C"));
+		Add(pHeatersDCUPowerH1 = new StdSwitch2(_sts, "HTRS/DCU PWR H1"));
+		Add(pHeatersDCUPowerH2 = new StdSwitch2(_sts, "HTRS/DCU PWR H2/DCU"));
+		Add(pHeatersDCUPowerH3 = new StdSwitch2(_sts, "HTRS/DCU PWR H3/DCU"));
+		Add(pAPDSPowerA = new StdSwitch2(_sts, "APDS PWR A"));
+		Add(pAPDSPowerB = new StdSwitch2(_sts, "APDS PWR B"));
+		Add(pAPDSPowerC = new StdSwitch2(_sts, "APDS PWR C"));
+		
+		
 		Add(pPyrosAp			= new StdSwitch2(_sts, "PYROS Ap"));
 		Add(pPyrosBp			= new StdSwitch2(_sts, "PYROS Bp"));
 		Add(pPyrosCp			= new StdSwitch2(_sts, "PYROS Cp"));
 
 		pControlPanelPowerA->SetLabel(0, "OFF");
 		pControlPanelPowerA->SetLabel(1, "ON");
+		
 		pControlPanelPowerB->SetLabel(0, "OFF");
 		pControlPanelPowerB->SetLabel(1, "ON");
+
 		pControlPanelPowerC->SetLabel(0, "OFF");
 		pControlPanelPowerC->SetLabel(1, "ON");
+
+
+		pHeatersDCUPowerH1->SetLabel(0, "OFF");
+		pHeatersDCUPowerH1->SetLabel(1, "ON");
+		pHeatersDCUPowerH2->SetLabel(0, "OFF");
+		pHeatersDCUPowerH2->SetLabel(1, "ON");
+		pHeatersDCUPowerH3->SetLabel(0, "OFF");
+		pHeatersDCUPowerH3->SetLabel(1, "ON");
+
+		pAPDSPowerA->SetLabel(0, "OFF");
+		pAPDSPowerA->SetLabel(1, "ON");
+		pAPDSPowerB->SetLabel(0, "OFF");
+		pAPDSPowerB->SetLabel(1, "ON");
+		pAPDSPowerC->SetLabel(0, "OFF");
+		pAPDSPowerC->SetLabel(1, "ON");
 	}
 
 	PanelA7A8ODS::~PanelA7A8ODS()
@@ -78,22 +105,62 @@ namespace vc {
 		//0.262413  2.443722  12.41595
 
 		pControlPanelPowerA->SetMouseRegion(0.081f, 0.124f, 0.094f, 0.197f);
-		pControlPanelPowerA->SetReference(_V(0.0, 2.443722, 12.41595), _V(-1.0, 0.0, 0.0));
+		pControlPanelPowerA->SetReference(_V(0.0, 2.443722, 12.41595),
+			_V(-1.0, 0.0, 0.0));
 		pControlPanelPowerA->DefineSwitchGroup(GRP_A8A3S1_ODSVC);
+
 		pControlPanelPowerB->SetMouseRegion(0.177f, 0.124f, 0.194f, 0.197f);
-		pControlPanelPowerB->SetReference(_V(0.0, 2.443722, 12.41595), _V(-1.0, 0.0, 0.0));
+		pControlPanelPowerB->SetReference(_V(0.0, 2.443722, 12.41595), 
+			_V(-1.0, 0.0, 0.0));
 		pControlPanelPowerB->DefineSwitchGroup(GRP_A8A3S2_ODSVC);
+
 		pControlPanelPowerC->SetMouseRegion(0.274f, 0.124f, 0.286f, 0.197f);
-		pControlPanelPowerC->SetReference(_V(0.0, 2.443722, 12.41595), _V(-1.0, 0.0, 0.0));
+		pControlPanelPowerC->SetReference(_V(0.0, 2.443722, 12.41595), 
+			_V(-1.0, 0.0, 0.0));
 		pControlPanelPowerC->DefineSwitchGroup(GRP_A8A3S3_ODSVC);
+
+		pHeatersDCUPowerH1->SetMouseRegion(0.076f, 0.337f, 0.095f, 0.379f);
+		pHeatersDCUPowerH1->SetReference(_V(0.0, 2.370605, 12.43914), 
+			_V(-1.0, 0.0, 0.0));
+		pHeatersDCUPowerH1->DefineSwitchGroup(GRP_A8A3S4_ODSVC);
+
+		pHeatersDCUPowerH2->SetMouseRegion(0.176f, 0.337f, 0.195f, 0.379f);
+		pHeatersDCUPowerH2->SetReference(_V(0.0, 2.370605, 12.43914),
+			_V(-1.0, 0.0, 0.0));
+		pHeatersDCUPowerH2->DefineSwitchGroup(GRP_A8A3S5_ODSVC);
+
+		pHeatersDCUPowerH3->SetMouseRegion(0.273f, 0.337f, 0.290f, 0.379f);
+		pHeatersDCUPowerH3->SetReference(_V(0.0, 2.370605, 12.43914), 
+			_V(-1.0, 0.0, 0.0));
+		pHeatersDCUPowerH3->DefineSwitchGroup(GRP_A8A3S6_ODSVC);
+
+		pAPDSPowerA->SetMouseRegion(0.076f, 0.519f, 0.095f, 0.590f);
+		pAPDSPowerA->SetReference(_V(0.0,  2.297854, 12.46215), _V(-1.0, 0.0, 0.0));
+		pAPDSPowerA->DefineSwitchGroup(GRP_A8A3S7_ODSVC);
+
+		pAPDSPowerB->SetMouseRegion(0.176f, 0.519f, 0.195f, 0.590f);
+		pAPDSPowerB->SetReference(_V(0.0,  2.297854, 12.46215), _V(-1.0, 0.0, 0.0));
+		pAPDSPowerB->DefineSwitchGroup(GRP_A8A3S8_ODSVC);
+
+		pAPDSPowerC->SetMouseRegion(0.273f, 0.519f, 0.290f, 0.590f);
+		pAPDSPowerC->SetReference(_V(0.0,  2.297854, 12.46215), _V(-1.0, 0.0, 0.0));
+		pAPDSPowerC->DefineSwitchGroup(GRP_A8A3S9_ODSVC);
 		
 		
 		pPyrosAp->SetMouseRegion(0.709f, 0.124f, 0.725f, 0.197f);
-		pPyrosAp->SetReference(_V(0.0, 2.443722, 12.41595), _V(1.0, 0.0, 0.0));
+		pPyrosAp->SetReference(_V(0.0, 2.443722, 12.41595),
+			_V(-1.0, 0.0, 0.0));
+		pPyrosAp->DefineSwitchGroup(GRP_A8A3S10_ODSVC);
+
 		pPyrosBp->SetMouseRegion(0.803f, 0.124f, 0.825f, 0.197f);
-		pPyrosBp->SetReference(_V(0.0, 2.443722, 12.41595), _V(1.0, 0.0, 0.0));
+		pPyrosBp->SetReference(_V(0.0, 2.443722, 12.41595), 
+			_V(-1.0, 0.0, 0.0));
+		pPyrosBp->DefineSwitchGroup(GRP_A8A3S11_ODSVC);
+
 		pPyrosCp->SetMouseRegion(0.902f, 0.124f, 0.920f, 0.197f);
-		pPyrosCp->SetReference(_V(0.0, 2.443722, 12.41595), _V(1.0, 0.0, 0.0));
+		pPyrosCp->SetReference(_V(0.0, 2.443722, 12.41595),
+			_V(-1.0, 0.0, 0.0));
+		pPyrosCp->DefineSwitchGroup(GRP_A8A3S12_ODSVC);
 
 	}
 
