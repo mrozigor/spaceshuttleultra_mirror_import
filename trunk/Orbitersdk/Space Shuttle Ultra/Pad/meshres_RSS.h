b6c13bff326505b21d62b5154bd5fc5548b637f5
orbiter_fan <orbiter_fan@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2008-12-13 18:39:08 +0000

    Fixed the FSS/RSS alignment issue. Requires a new meshres which is included and tested.

--- trunk/Orbitersdk/Space Shuttle Ultra/Pad/meshres_RSS.h
+++ trunk/Orbitersdk/Space Shuttle Ultra/Pad/meshres_RSS.h
@@ -1,26 +1,26 @@
 // ========================================================
-// Mesh resource file for ..\..\Meshes\SSU\LC39A_RSS.msh
-// Generated with meshc on Wed Dec 10 21:16:12 2008
+// Mesh resource file for LC39A_RSS.msh
+// Generated with meshc on Sat Dec 13 19:36:09 2008
 
 // ========================================================
 
 // Number of mesh groups:
-#define NGRP_RSS 46
+#define NGRP 46
 
 // Number of materials:
-#define NMAT_RSS 7
+#define NMAT 7
 
 // Number of textures:
-#define NTEX_RSS 1
+#define NTEX 1
 
 // Named mesh groups:
-#define GRP_Cylinder01 0
-#define GRP_Cylinder358 1
-#define GRP_Cylinder382 2
-#define GRP_Cylinder389 3
-#define GRP_Minus_Y_Wing_cover 4
-#define GRP_Box40 5
-#define GRP_PCR_doors 6
+#define GRP_Minus_Y_Wing_cover 0
+#define GRP_Cylinder01 1
+#define GRP_Cylinder358 2
+#define GRP_Cylinder382 3
+#define GRP_Cylinder389 4
+#define GRP_Box01 5
+#define GRP_Box40 6
 #define GRP_Box64 7
 #define GRP_Box73 8
 #define GRP_Box140 9
@@ -30,33 +30,33 @@
 #define GRP_Box274 13
 #define GRP_Box281 14
 #define GRP_Box283 15
-#define GRP_Box330 16
-#define GRP_Upper_catwalks_and_stairs 17
-#define GRP_Box527 18
-#define GRP_Box535 19
-#define GRP_Box565 20
-#define GRP_Cylinder248 21
-#define GRP_Cylinder314 22
-#define GRP_Cylinder316 23
-#define GRP_Cylinder350 24
-#define GRP_Cylinder370 25
-#define GRP_Cylinder381 26
-#define GRP_Main 27
-#define GRP_Object01 28
-#define GRP_PCR_access_doors 29
-#define GRP_RSS 30
-#define GRP_RSS_Plus_Y_OWP_Lower 31
-#define GRP_Tube08 32
-#define GRP_Vertical_torque_tubes 33
-#define GRP_Cylinder221 34
-#define GRP_OMBUU 35
-#define GRP_Cylinder90 36
-#define GRP_Object02 37
-#define GRP_RSS_truss_structure 38
-#define GRP_Box321 39
-#define GRP_Box323 40
-#define GRP_Box324 41
-#define GRP_Line07 42
-#define GRP_Box284 43
-#define GRP_Box01 44
+#define GRP_Box284 16
+#define GRP_Box321 17
+#define GRP_Box323 18
+#define GRP_Box324 19
+#define GRP_Box330 20
+#define GRP_Box527 21
+#define GRP_Box535 22
+#define GRP_Box565 23
+#define GRP_Cylinder90 24
+#define GRP_Cylinder221 25
+#define GRP_Cylinder248 26
+#define GRP_Cylinder314 27
+#define GRP_Cylinder316 28
+#define GRP_Cylinder350 29
+#define GRP_Cylinder370 30
+#define GRP_Cylinder381 31
+#define GRP_Line07 32
+#define GRP_Main 33
+#define GRP_Object01 34
+#define GRP_Object02 35
+#define GRP_OMBUU 36
+#define GRP_PCR_access_doors 37
+#define GRP_PCR_doors 38
+#define GRP_RSS 39
+#define GRP_RSS_Plus_Y_OWP_Lower 40
+#define GRP_RSS_truss_structure 41
+#define GRP_Tube08 42
+#define GRP_Upper_catwalks_and_stairs 43
+#define GRP_Vertical_torque_tubes 44
 //#define GRP_RSS 45
