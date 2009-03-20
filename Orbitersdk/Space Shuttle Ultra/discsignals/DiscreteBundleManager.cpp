2093b638ce6eb4e518cfe456543f3708d2948897
Urwumpe <Urwumpe@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2009-03-20 07:49:51 +0000

    More debug info for CreateBundle

--- Orbitersdk/Space Shuttle Ultra/discsignals/DiscreteBundleManager.cpp
+++ Orbitersdk/Space Shuttle Ultra/discsignals/DiscreteBundleManager.cpp
@@ -9,7 +9,7 @@ DiscreteBundleManager::DiscreteBundleManager(void)
 
 DiscreteBundleManager::~DiscreteBundleManager(void)
 {
-	for(int i=0;i<bundles.size();i++) {
+	for(unsigned int i=0;i<bundles.size();i++) {
 		DiscreteBundle* pBundle=bundles[i];
 		delete pBundle;
 	}
@@ -28,7 +28,9 @@ bool DiscreteBundleManager::AddBundle(discsignals::DiscreteBundle *pBundle)
 
 DiscreteBundle* DiscreteBundleManager::CreateBundle(const std::string &_ident, unsigned short usLines)
 {
-	oapiWriteLog("(DiscreteBundleManager::CreateBundle) Enter");
+	char pszBuffer[400];
+	sprintf_s(pszBuffer, 400, "(DiscreteBundleManager::CreateBundle) Enter \"%s\"", _ident.c_str());
+	oapiWriteLog(pszBuffer);
 	if(ExistsBundle(_ident))
 	{
 		oapiWriteLog("(DiscreteBundleManager::CreateBundle) Found Bundle");
