317deee7e8b2746e55e43b5f7ba743bdabbbf886
Urwumpe <Urwumpe@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2010-03-24 23:26:15 +0000

    Intermediate release, fixing some compilation bugs.

--- trunk/Orbitersdk/Space Shuttle Ultra/dps/BIU.cpp
+++ trunk/Orbitersdk/Space Shuttle Ultra/dps/BIU.cpp
@@ -1,22 +1,19 @@
+#include "dps_defs.h"
 #include "BIU.h"
+#include "ShuttleBus.h"
 
 namespace dps {
 
 	BIU::BIU() 
-		: label(), pParent(NULL)
+		: label(), pParent(NULL), m_owner(NULL)
 	{
 		usTerminalID = 0xFFFF;
-		bIsBusMaster = false;
-		bDontListen = true;
-		
 	}
 
-	BIU::BIU(unsigned short usTerminalID, bool bIsMaster) 
-		: label(), pParent(NULL)
+	BIU::BIU(unsigned short usTerminalID) 
+		: label(), pParent(NULL), m_owner(NULL)
 	{
 		this->usTerminalID = usTerminalID;
-		bIsBusMaster = bIsMaster;
-		bDontListen = true;
 		
 	}
 
@@ -38,29 +35,32 @@ namespace dps {
 	AtlantisSubsystem* BIU::GetParent() const {
 		return pParent;
 	}
-	
+
+	bool BIU::IsController() const
+	{
+		return false;
+	}
+
+	bool BIU::IsTerminal() const
+	{
+		return false;
+	}
+
+
 	void BIU::SetTerminalID(unsigned short TermID) {
 		usTerminalID = TermID;
 	}
 
-	void BIU::Init(AtlantisSubsystem* pSubsys, const string& _label, 
-		unsigned short usTermID, bool bIsMaster) {
+	void BIU::Init(AtlantisSubsystem* pSubsys,
+		IConnectedToBus* owner,
+		const string& _label, 
+		unsigned short usTermID) {
 
 			SetParent(pSubsys);
 			Label(_label);
 			SetTerminalID(usTermID);
-			bIsBusMaster = bIsMaster;
 	}
-
-	void BIU::OnNewMessage() {
-	}
-
-	void BIU::SendFloat(unsigned short usTarget, float fValue) {
-	}
-
-	void BIU::SendWord(unsigned short usTarget, short sWord) {
-	}
-
+	
 	void BIU::ConnectTo(ShuttleBus* _pBus) {
 		//char pszBuffer[255];
 		pBus = _pBus;
