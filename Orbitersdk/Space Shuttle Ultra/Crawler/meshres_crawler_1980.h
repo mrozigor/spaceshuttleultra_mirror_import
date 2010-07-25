e56d52de80467aecf861cfbdf8d56e28444824cd
daves-ssu <daves-ssu@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2010-07-25 12:28:36 +0000

    Updated 1980's C/T mesh along with a meshres file.

--- Orbitersdk/Space Shuttle Ultra/Crawler/meshres_crawler_1980.h
+++ Orbitersdk/Space Shuttle Ultra/Crawler/meshres_crawler_1980.h
@@ -1,112 +1,14 @@
 // ========================================================
-// Mesh resource file for Crawler_mainbody_1980.msh
-// Generated with meshc on Mon Jun 07 15:01:36 2010
+// Mesh resource file for Crawler_mainboady_1980.msh
+// Generated with meshc on Sun Jul 25 14:26:31 2010
 
 // ========================================================
 
 // Number of mesh groups:
-#define NGRP 96
+#define NGRP 0
 
 // Number of materials:
-#define NMAT 6
+#define NMAT 0
 
 // Number of textures:
-#define NTEX 3
-
-// Named mesh groups:
-#define GRP_Box06 0
-#define GRP_Box77 1
-#define GRP_Cylinder02 2
-#define GRP_Box82 3
-#define GRP_Cylinder192 4
-#define GRP_Line27 5
-#define GRP_Box22 6
-#define GRP_Box63 7
-#define GRP_Box532 8
-#define GRP_Box613 9
-#define GRP_Box75 10
-#define GRP_Box78 11
-#define GRP_Box86 12
-#define GRP_Line01 13
-#define GRP_Box88 14
-#define GRP_Hydraulic_Oil_Tank 15
-#define GRP_Line03 16
-#define GRP_Box105 17
-#define GRP_Cylinder10 18
-#define GRP_Box134 19
-#define GRP_Box186 20
-#define GRP_Box02 21
-#define GRP_Box01 22
-#define GRP_Box712 23
-#define GRP_Box713 24
-#define GRP_Box101 25
-#define GRP_Box239 26
-#define GRP_Line10 27
-#define GRP_Box04 28
-#define GRP_Box03 29
-#define GRP_Box30 30
-#define GRP_Cylinder06 31
-#define GRP_Box09 32
-#define GRP_Cylinder261 33
-#define GRP_Box791 34
-#define GRP_Cylinder49 35
-#define GRP_Box792 36
-#define GRP_Box793 37
-#define GRP_Cylinder262 38
-#define GRP_Box794 39
-#define GRP_Cylinder263 40
-#define GRP_Box658 41
-#define GRP_Cylinder01 42
-#define GRP_Box05 43
-#define GRP_Box08 44
-#define GRP_Corner_D_Rear_steering_cylinder 45
-#define GRP_Box354 46
-#define GRP_Corner_D_Auxillary_steering_arm 47
-#define GRP_Corner_D_Rear_bottom_steering_cylinder_dust_shield 48
-#define GRP_Corner_D_FWD_bottom_steering_cylinder_dust_shield 49
-#define GRP_CornerDC_FWD_steering_cylinder 50
-#define GRP_Box353 51
-#define GRP_Corner_A_FWD_steering_cylinder 52
-#define GRP_Box349 53
-#define GRP_Corner_A_FWD_bottom_steering_cylinder_dust_sheild 54
-#define GRP_Corner_A_Auxillary_steering_arm 55
-#define GRP_Corner_A_Rear_bottom_steering_cylinder_dust_shield 56
-#define GRP_Box348 57
-#define GRP_Corner_A_Rear_steering_cylinder 58
-#define GRP_Corner_A_FWD_steering_cylinder_dust_shield 59
-#define GRP_Corner_A_Rear_steering_cylinder_dust_shield 60
-#define GRP_Corner_D_FWD_steering_cylinder_dust_shield 61
-#define GRP_Corner_D_Rear_steering_cylinder_dust_shield 62
-#define GRP_Corner_D_FWD_bottom_steering_cylinder 63
-#define GRP_Corner_A_Rear_bottom_steering_cylinder 64
-#define GRP_Corner_A_FWD_bottom_steering_cylinder 65
-#define GRP_Corner_D_rear_bottom_steering_cylinder 66
-#define GRP_Corner_B_Rear_steering_cylinder_dust_shield 67
-#define GRP_Corner_C_FWD_steering_cylinder_dust_shield 68
-#define GRP_Corner_B_FWD_steering_cylinder_dust_shield 69
-#define GRP_Corner_C_FWD_steering_cylinder_dust_shield 70
-#define GRP_Corner_C_FWD_steering_cylinder 71
-#define GRP_Corner_B_FWD_steering_cylinder 72
-#define GRP_Corner_B_Rear_steering_cylinder 73
-#define GRP_Corner_C_Rear_steering_cylinder 74
-#define GRP_Box230 75
-#define GRP_Box343 76
-#define GRP_Box344 77
-#define GRP_Box229 78
-#define GRP_Corner_B_FWD_bottom_steering_cylinder 79
-#define GRP_Corner_B_Rear_bottom_steering_cylinder_dust_sheild 80
-#define GRP_Corner_C_Rear_bottom_steering_cylinder 81
-#define GRP_Cylinder502 82
-#define GRP_Corner_B_FWD_bottom_steering_cylinder_dust_shield 83
-#define GRP_Corner_C_FWD_bottom_steering_cylinder 84
-#define GRP_Cylinder506 85
-#define GRP_Corner_B_Rear_bottom_steering_cylinder 86
-#define GRP_Corner_C_Auxillary_steering_arm 87
-#define GRP_Corner_B_Auxillary_steering_arm 88
-#define GRP_Box342 89
-#define GRP_Box84 90
-#define GRP_Box41 91
-#define GRP_Box07 92
-#define GRP_Box196 93
-#define GRP_Box714 94
-#define GRP_Line02 95
+#define NTEX 0
