49bb1a43889efbf03b81cbc736cd7c1026e79260
siamesecat <siamesecat@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2010-09-25 20:43:44 +0000

    Updating XenonLights to target shuttle instead of launch pad.

--- Orbitersdk/Space Shuttle Ultra/Xenon_Lights/Xenon_Lights.cpp
+++ Orbitersdk/Space Shuttle Ultra/Xenon_Lights/Xenon_Lights.cpp
@@ -3,11 +3,15 @@
 #include "Xenon_Lights.h"
 
 XenonLights::XenonLights(OBJHANDLE hVessel, int fmodel)
-: VESSEL3(hVessel, fmodel), bLightsOn(false), bFoundTarget(false)
+: VESSEL3(hVessel, fmodel), bLightsOn(false), 
+bFoundTarget(false), hTarget(NULL),
+updateClock(-0.1)
 {
 	phLights = NULL;
 	thLights[0] = thLights[1] = NULL;
 	pLights[0] = pLights[1] = NULL;
+
+	lightDir = _V(0, 0, 1);
 }
 
 XenonLights::~XenonLights()
@@ -28,16 +32,16 @@ void XenonLights::CreateLights()
 	VECTOR3 LIGHT_POS[2] = { _V(2.395, 1.526, 0.488), _V(-2.395, 1.526, 0.488)};
 
 	const COLOUR4 LIGHT_DIFFUSE = {0.925, 1, 0.925, 1};
-	const COLOUR4 LIGHT_SPECULAR = {0.925, 1, 0.925, 1};
+	const COLOUR4 LIGHT_SPECULAR = {0.425, 0.5, 0.425, 0.5};
 	const COLOUR4 LIGHT_AMBIENT = {0, 0, 0, 0};
 	const double LIGHT_RANGE = 1000.0;
 	const double LIGHT_ATT0 = 1e-3;
 	const double LIGHT_ATT1 = 0;
-	const double LIGHT_ATT2 = 5e-5;
+	const double LIGHT_ATT2 = 1e-5;
 
 	for(int i=0;i<2;i++) {
 		pLights[i] = AddSpotLight(LIGHT_POS[i], _V(0, 0, 1),
-			LIGHT_RANGE, LIGHT_ATT0, LIGHT_ATT1, LIGHT_ATT2, 3.0*RAD, 7.5*RAD,
+			LIGHT_RANGE, LIGHT_ATT0, LIGHT_ATT1, LIGHT_ATT2, 3.0*RAD, 5.0*RAD,
 			LIGHT_DIFFUSE, LIGHT_SPECULAR, LIGHT_AMBIENT);
 
 		// create fake thruster to simulate glare from lights
@@ -46,35 +50,74 @@ void XenonLights::CreateLights()
 	}
 }
 
-void XenonLights::clbkPostCreation()
+void XenonLights::clbkPreStep(double simT, double simDT, double MJD)
 {
-	const double MAX_DIST = 1000.0; // max distance from light to shuttle
-	const double HEIGHT_OFFSET = 75.0;
+	const double HEIGHT_OFFSET = 70.0;
 
+	updateClock -= simDT;
+	if(updateClock < 0.0) {
+		updateClock = 60.0;
+
+		// if target has not been found previously, try again
+		if(bFoundTarget || FindTarget()) {
+			// make sure target is still within range
+			VECTOR3 relPos;
+			GetRelativePos(hTarget, relPos);
+			if(length(relPos) > MAX_TARGET_RANGE) {
+				bFoundTarget = false;
+				hTarget = NULL;
+			}
+			else {
+				MATRIX3 RotMatrix;
+				GetRotationMatrix(RotMatrix);
+				VECTOR3 pos = tmul(RotMatrix, -relPos);
+				pos.y += HEIGHT_OFFSET;
+				lightDir=pos/length(pos);
+			}
+		}
+
+		for(int i=0;i<2;i++) {
+			pLights[i]->SetDirection(lightDir);
+		}
+		SetLightState(true);
+	}
+}
+
+bool XenonLights::FindTarget()
+{
 	DWORD vesselCount = oapiGetVesselCount();
 	for(int i=0;i<vesselCount;i++) {
 		OBJHANDLE hV = oapiGetVesselByIndex(i);
 		VESSEL* v = oapiGetVesselInterface(hV);
-		if(!_stricmp(v->GetClassName(), PAD_CLASS_NAME)) {
-			VECTOR3 relPos;
-			oapiGetRelativePos(hV, GetHandle(), &relPos);
-			if(length(relPos) < MAX_DIST) {
-				bFoundTarget = true;
+		if(!_stricmp(v->GetClassName(), SHUTTLE_CLASS_NAME)) {
+			OBJHANDLE hParent = GetAttachedParent(hV);
+			while(hParent) {
+				hV = hParent;
+				hParent = GetAttachedParent(hV);
+			}
 
-				MATRIX3 RotMatrix;
-				GetRotationMatrix(RotMatrix);
-				VECTOR3 pos = tmul(RotMatrix, relPos);
-				pos.y+=HEIGHT_OFFSET;
-				lightDir=pos/length(pos);
-				break;
+			VECTOR3 relPos;
+			GetRelativePos(hV, relPos);
+			if(length(relPos) < MAX_TARGET_RANGE) {
+				bFoundTarget = true;
+				hTarget = hV;
+				return true;
 			}
 		}
 	}
+	return false;
+}
 
-	for(int i=0;i<2;i++) {
-		pLights[i]->SetDirection(lightDir);
+OBJHANDLE XenonLights::GetAttachedParent(OBJHANDLE hVessel) const
+{
+	VESSEL* pVessel = oapiGetVesselInterface(hVessel);
+	DWORD count = pVessel->AttachmentCount(true);
+	for(DWORD i=0;i<count;i++) {
+		ATTACHMENTHANDLE ah = pVessel->GetAttachmentHandle(true, i);
+		OBJHANDLE hV = pVessel->GetAttachmentStatus(ah);
+		if(hV) return hV;
 	}
-	SetLightState(true);
+	return NULL;
 }
 
 void XenonLights::SetLightState(bool on) const
