f641e45d89e1c05770de9fa8e6da38759853a1f0
siamesecat <siamesecat@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2010-12-21 20:41:54 +0000

    Adding startup/shutdown sounds for Crawler engine. Changing acceleration/deceleration keys (to change speed instead of velocity; direction is base on FWD/REV PBI selection). Changing CrawlerEngine class internals to have engine state (off, on, start, shutdown).

--- trunk/Orbitersdk/Space Shuttle Ultra/Crawler/CrawlerEngine.cpp
+++ trunk/Orbitersdk/Space Shuttle Ultra/Crawler/CrawlerEngine.cpp
@@ -3,7 +3,8 @@
 #include <OrbiterSoundSDK35.h>
 
 CrawlerEngine::CrawlerEngine(SubsystemDirector<Crawler>* _director)
-: Subsystem(_director, "Engine")
+: Subsystem(_director, "Engine"),
+engineState(OFF)
 {
 	targetSpeed = 0.0;
 	currentAcceleration = 0.0;
@@ -50,48 +51,58 @@ void CrawlerEngine::Realize()
 	commandVoltage.Connect(pBundle, 0);
 	commandVoltage.SetLine(0.0f);
 	for(unsigned int i=0;i<3;i++)
-		engineState[i].Connect(pBundle, i+1);
+		engineDirection[i].Connect(pBundle, i+1);
 }
 
 void CrawlerEngine::OnPreStep(double SimT, double SimDT, double MJD)
 {
-	// calculate target velocity
+	// set target speed
 	if(increaseTgtSpeed) {
-		double dv = SimDT * 0.1;
-		if (targetSpeed < 0 && targetSpeed + dv >= 0) {
-			targetSpeed = 0;
-			increaseTgtSpeed=decreaseTgtSpeed=false;
-		} else {
-			targetSpeed += dv;
-		}
+		double dv = SimDT*0.1;
+		targetSpeed = range(0.0, targetSpeed+dv, maxSpeed);
 	}
 	else if(decreaseTgtSpeed) {
-		double dv = SimDT * -0.1;
-		if (targetSpeed > 0 && targetSpeed + dv <= 0) {
-			targetSpeed = 0;
-			decreaseTgtSpeed=increaseTgtSpeed=false;
-		} else {
-			targetSpeed += dv;
+		double dv = SimDT*-0.1;
+		targetSpeed = range(0.0, targetSpeed+dv, maxSpeed);
+	}
+
+	// use FWD/NEUT/REV PBIs to start/shutdown engine and play correct sounds
+	// use startup/shutdown sound length to determine when startup or shutdown is complete
+	if(engineDirection[FWD] || engineDirection[REV]) {
+		if(engineState==OFF || engineState==SHUTDOWN) {
+			engineState = STARTING;
+			PlayVesselWave3(V()->GetSoundID(), ENGINE_START_SOUND_ID);
+		}
+		else if(engineState==STARTING && !IsPlaying3(V()->GetSoundID(), ENGINE_START_SOUND_ID)) {
+			engineState = ON;
+		}
+		else if(engineState==ON) {
+			PlayVesselWave3(V()->GetSoundID(), ENGINE_SOUND_ID, LOOP);
 		}
 	}
-	if(engineState[FWD])
-		targetSpeed = range(0.0, targetSpeed, maxSpeed);
-	else if(engineState[REV])
-		targetSpeed = range(-maxSpeed, targetSpeed, 0.0);
-	else
-		targetSpeed = 0.0;
-
+	else { // NEUT
+		if(engineState==ON || engineState==STARTING) {
+			engineState = SHUTDOWN;
+			PlayVesselWave3(V()->GetSoundID(), ENGINE_STOP_SOUND_ID);
+			StopVesselWave3(V()->GetSoundID(), ENGINE_SOUND_ID);
+		}
+		else if(engineState==SHUTDOWN && !IsPlaying3(V()->GetSoundID(), ENGINE_STOP_SOUND_ID)) {
+			engineState = OFF;
+		}
+	}
+	
 	// simplistic implementation of engine: acc is proportional to engine power
-	if((engineState[FWD] && targetSpeed>0.0) || (engineState[REV] && targetSpeed<0.0)) {
-		currentAcceleration = enginePower*0.01;
-
+	if(engineState==ON) {
 		// calculate throttle setting
 		double dPower;
 		double tgtPower;
 		double actualSpeed = currentSpeed.GetVoltage();
 		enginePower = fabs(enginePower); // for calculations, use positive power
 		if(!Eq(targetSpeed, 0.0, 0.01)) {
-			double err=targetSpeed-actualSpeed;
+			//double err=targetSpeed-actualSpeed;
+			double err=0.0;
+			if(engineDirection[FWD]) err=targetSpeed-actualSpeed;
+			else err=-targetSpeed-actualSpeed;
 			if(fabs(err) > 0.005) {
 				tgtPower = range(0.30, fabs(err)*10.0, 1.0);
 				if((actualSpeed>0.0 && err<0.0) || (actualSpeed<0.0 && err>0.0)) {
@@ -115,37 +126,32 @@ void CrawlerEngine::OnPreStep(double SimT, double SimDT, double MJD)
 		if(abs(dPower) > abs(tgtPower-enginePower)) enginePower = tgtPower;
 		else enginePower = range(0.0, enginePower+dPower, 1.0);
 		// correct sign of enginePower
-		if(targetSpeed < 0.0) enginePower = -enginePower;
+		if(engineDirection[REV]) enginePower = -enginePower;
 	}
 	else {
 		enginePower = 0.0;
 	}
-
+	currentAcceleration = enginePower*0.01;
 	commandVoltage.SetLine(enginePower);
-	//sprintf_s(oapiDebugString(), 255, "Tgt Velocity: %f Power: %f", targetSpeed*MPS2MPH, enginePower);
 
-	//play sounds
-	if(engineState[FWD] || engineState[REV]) {
-		PlayVesselWave3(V()->GetSoundID(), ENGINE_SOUND_ID, LOOP);
-	}
-	else {
-		StopVesselWave3(V()->GetSoundID(), ENGINE_SOUND_ID);
-	}
+	sprintf_s(oapiDebugString(), 255, "Target Speed: %f", targetSpeed);
 }
 
 bool CrawlerEngine::OnParseLine(const char* keyword, const char* line)
 {
 	std::string strKey = keyword;
-	/*if(strKey == "VELOCITY") {
-		sscanf_s(line, "%lf", &actualSpeed);
-		return true;
-	}
-	else */if(strKey == "TGT_VELOCITY") {
+	if(strKey == "TGT_VELOCITY") {
 		sscanf_s(line, "%lf", &targetSpeed);
 		return true;
 	}
 	else if(strKey == "ENGINE_POWER") {
 		sscanf_s(line, "%lf", &enginePower);
+		return true;
+	}
+	else if(strKey == "ENGINE_STATE") {
+		int temp;
+		sscanf_s(line, "%d", &temp);
+		engineState = static_cast<ENGINE_STATE>(temp);
 		return true;
 	}
 	return false;
@@ -155,5 +161,6 @@ void CrawlerEngine::OnSaveState(FILEHANDLE scn) const
 {
 	//oapiWriteScenario_float(scn, "VELOCITY", actualSpeed);
 	oapiWriteScenario_float(scn, "TGT_VELOCITY", targetSpeed);
-	oapiWriteScenario_float(scn, "ENGINE_POWER", enginePower);	
+	oapiWriteScenario_float(scn, "ENGINE_POWER", enginePower);
+	oapiWriteScenario_int(scn, "ENGINE_STATE", engineState);
 }
