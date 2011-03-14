76d9b4f9fe1093f1c2515adc858c08d106362fcf
daves-ssu <daves-ssu@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2011-03-14 17:15:32 +0000

    Some Crawler VC check-ins(new meshres file)

--- Orbitersdk/Space Shuttle Ultra/Crawler/meshres_crawler_vc.h
+++ Orbitersdk/Space Shuttle Ultra/Crawler/meshres_crawler_vc.h
@@ -1,57 +1,119 @@
-// ======================================================
-// Created by ssumeshc 0.1
-// Input file: ../../../Meshes/SSU/Crawler_VC_panels.msh
-// ======================================================
+// ========================================================
+// Mesh resource file for Crawler_VC_panels.msh
+// Generated with meshc on Mon Mar 14 18:14:32 2011
 
-#pragma once
+// ========================================================
 
-const unsigned int NUMGRP_VC = 47;
+// Number of mesh groups:
+#define NGRP_VC 103
 
-const unsigned int GRP_PLANE10_VC	=	0;	//Plane10
-const unsigned int GRP_GROUP_10_VC	=	1;	//Group 10
-const unsigned int GRP_GROUP_5_VC	=	2;	//Group 5
-const unsigned int GRP_GROUP_31_VC	=	3;	//Group 31
-const unsigned int GRP_GROUP_8_VC	=	4;	//Group 8
-const unsigned int GRP_GROUP_46_VC	=	5;	//Group 46
-const unsigned int GRP_OBJECT02_VC	=	6;	//Object02
-const unsigned int GRP_GROUP_14_VC	=	7;	//Group 14
-const unsigned int GRP_SPEEDOMETERGAUGE_VC	=	8;	//SPEEDOMETER_GAUGE
-const unsigned int GRP_MESH02_VC	=	9;	//Mesh02
-const unsigned int GRP_OBJECT01_VC	=	10;	//Object01
-const unsigned int GRP_MESH01_VC	=	11;	//Mesh01
-const unsigned int GRP_GROUP_1_VC	=	12;	//Group 1
-const unsigned int GRP_BOX01_VC	=	13;	//Box01
-const unsigned int GRP_STEERINGACTUALLEFTGAUGE_VC	=	14;	//STEERING_ACTUAL_LEFT_GAUGE
-const unsigned int GRP_GROUP_52_VC	=	15;	//Group 52
-const unsigned int GRP_STEERINGDESIREDGAUGE_VC	=	16;	//STEERING_DESIRED_GAUGE
-const unsigned int GRP_GROUP_56_VC	=	17;	//Group 56
-const unsigned int GRP_STEERINGACTUALRIGHTGAUGE_VC	=	18;	//STEERING_ACTUAL_RIGHT_GAUGE
-const unsigned int GRP_GROUP_54_VC	=	19;	//Group 54
-const unsigned int GRP_AVERAGEHEIGHTGAUGE_VC	=	20;	//AVERAGE_HEIGHT_GAUGE
-const unsigned int GRP_GROUP_58_VC	=	21;	//Group 58
-const unsigned int GRP_CABINCONTROLINDICATOR_VC	=	22;	//CAB_IN_CONTROL_INDICATOR
-const unsigned int GRP_CYLINDER11_VC	=	23;	//Cylinder11
-const unsigned int GRP_BOX08_VC	=	24;	//Box08
-const unsigned int GRP_LDSDISPLAYBOX_VC	=	25;	//LDS_display_box
-const unsigned int GRP_CYLINDER01_VC	=	26;	//Cylinder01
-const unsigned int GRP_CYLINDER02_VC	=	27;	//Cylinder02
-const unsigned int GRP_CYLINDER06_VC	=	28;	//Cylinder06
-const unsigned int GRP_CYLINDER04_VC	=	29;	//Cylinder04
-const unsigned int GRP_CYLINDER05_VC	=	30;	//Cylinder05
-const unsigned int GRP_CYLINDER03_VC	=	31;	//Cylinder03
-const unsigned int GRP_EXCITERONINDICATOR_VC	=	32;	//EXCITER_ON_INDICATOR
-const unsigned int GRP_MOTORFIELDLOSSINDICATOR_VC	=	33;	//MOTOR_FIELD_LOSS_INDICATOR
-const unsigned int GRP_GROUNDFAULTINDICATOR_VC	=	34;	//GROUND_FAULT_INDICATOR
-const unsigned int GRP_BRAKESONINDICATOR_VC	=	35;	//BRAKES_ON_INDICATOR
-const unsigned int GRP_SPEEDPBI_VC	=	36;	//Speed_PBI
-const unsigned int GRP_FWDPBI_VC	=	37;	//FWD_PBI
-const unsigned int GRP_REVPBI_VC	=	38;	//REV_PBI
-const unsigned int GRP_NEUTPBI_VC	=	39;	//NEUT_PBI
-const unsigned int GRP_BRAKESPBI_VC	=	40;	//BRAKES_PBI
-const unsigned int GRP_MAXANGLEWARNINGINDICATOR_VC	=	41;	//MAX_ANGLE_WARNING_INDICATOR
-const unsigned int GRP_BOX05_VC	=	42;	//Box05
-const unsigned int GRP_CABSELECTEDINDICATOR_VC	=	43;	//CAB_SELECTED_INDICATOR
-const unsigned int GRP_COMMANDVOLTAGEGAUGE_VC	=	44;	//COMMAND_VOLTAGE_GAUGE
-const unsigned int GRP_CYLINDER07_VC	=	45;	//Cylinder07
-const unsigned int GRP_BOX03_VC	=	46;	//Box03
+// Number of materials:
+#define NMAT_VC 9
 
+// Number of textures:
+#define NTEX_VC 2
+
+// Named mesh groups:
+#define GRP_Plane10_VC 0
+#define GRP_Group_46_VC 1
+#define GRP_Object02_VC 2
+#define GRP_Group_VC 3
+#define GRP_SPEEDOMETER_GAUGE_VC 4
+#define GRP_Mesh02_VC 5
+#define GRP_Seat_01_VC 6
+#define GRP_Seat_02_VC 7
+#define GRP_Seat_03_VC 8
+#define GRP_Seat_04_VC 9
+#define GRP_Line04_VC 10
+#define GRP_Line01_VC 11
+#define GRP_Line02_VC 12
+#define GRP_Line03_VC 13
+#define GRP_Plane02_VC 14
+#define GRP_Plane03_VC 15
+#define GRP_Plane04_VC 16
+#define GRP_Plane05_VC 17
+#define GRP_Plane06_VC 18
+#define GRP_Plane07_VC 19
+#define GRP_Line05_VC 20
+#define GRP_Plane08_VC 21
+#define GRP_LDS_cab_switch_VC 22
+#define GRP_Alignment_bar_VC 23
+#define GRP_LDS_display_box_VC 24
+#define GRP_Cylinder01_VC 25
+#define GRP_Cylinder02_VC 26
+#define GRP_Cylinder06_VC 27
+#define GRP_Cylinder04_VC 28
+#define GRP_LDS_sense_01_knob_VC 29
+#define GRP_LDS_sense_10_knob_VC 30
+#define GRP_LDS_sense_1_knob_VC 31
+#define GRP_Box10_VC 32
+#define GRP_Object03_VC 33
+#define GRP_Box02_VC 34
+#define GRP_LDS_mode_switch_VC 35
+#define GRP_Box09_VC 36
+#define GRP_LDS_SET_switch_VC 37
+#define GRP_Cylinder07_VC 38
+#define GRP_Cylinder03_VC 39
+#define GRP_Cylinder05_VC 40
+#define GRP_Box08_VC 41
+#define GRP_Cylinder11_VC 42
+#define GRP_Box03_VC 43
+#define GRP_AVERAGE_HEIGHT_GAUGE_VC 44
+#define GRP_Box05_VC 45
+#define GRP_Box06_VC 46
+#define GRP_Box11_VC 47
+#define GRP_Box12_VC 48
+#define GRP_Box13_VC 49
+#define GRP_Box14_VC 50
+#define GRP_Box15_VC 51
+#define GRP_Box16_VC 52
+#define GRP_CAB_ACK_PBI_VC 53
+#define GRP_CAB_IN_CONTROL_INDICATOR_VC 54
+#define GRP_CAB_SELECTED_INDICATOR_VC 55
+#define GRP_CRAB_MODE_PBI_VC 56
+#define GRP_Cylinder08_VC 57
+#define GRP_Cylinder09_VC 58
+#define GRP_Cylinder10_VC 59
+#define GRP_Cylinder12_VC 60
+#define GRP_Cylinder13_VC 61
+#define GRP_Cylinder14_VC 62
+#define GRP_Cylinder15_VC 63
+#define GRP_Cylinder16_VC 64
+#define GRP_Cylinder17_VC 65
+#define GRP_Cylinder19_VC 66
+#define GRP_Cylinder20_VC 67
+#define GRP_Cylinder21_VC 68
+#define GRP_Cylinder22_VC 69
+#define GRP_Cylinder23_VC 70
+#define GRP_Cylinder24_VC 71
+#define GRP_Cylinder25_VC 72
+#define GRP_FAULT_INDICATOR01_VC 73
+#define GRP_GCIRC_MODE_PBI_VC 74
+#define GRP_INDEP_MODE_PBI_VC 75
+#define GRP_MAX_ANGLE_WARNING_INDICATOR02_VC 76
+#define GRP_MODE_PBI_VC 77
+#define GRP_MOTOR_CHKS_INDICATOR_VC 78
+#define GRP_STEERING_ACTUAL_LEFT_GAUGE_VC 79
+#define GRP_STEERING_ACTUAL_RIGHT_GAUGE_VC 80
+#define GRP_STEERING_DESIRED_GAUGE_VC 81
+#define GRP_SPEED_PBI_VC 82
+#define GRP_FWD_PBI_VC 83
+#define GRP_NEUT_PBI_VC 84
+#define GRP_REV_PBI_VC 85
+#define GRP_EXCITER_ON_INDICATOR_VC 86
+#define GRP_MOTOR_FIELD_LOSS_INDICATOR_VC 87
+#define GRP_GROUND_FAULT_INDICATOR_VC 88
+#define GRP_LAMP_TEST_PBI_VC 89
+#define GRP_BRAKES_PBI_VC 90
+#define GRP_COMMAND_VOLTAGE_GAUGE_VC 91
+#define GRP_Box04_VC 92
+#define GRP_SPEED_CONTROL_KNOB_VC 93
+#define GRP_Box01_VC 94
+#define GRP_BRAKES_ON_INDICATOR_VC 95
+#define GRP_Mesh01_VC 96
+#define GRP_Object01_VC 97
+#define GRP_Group_VC 98
+#define GRP_Group_10_VC 99
+#define GRP_Group_5_VC 100
+#define GRP_Steering_wheel_degrees_VC 101
+#define GRP_Steering_wheel_VC 102
