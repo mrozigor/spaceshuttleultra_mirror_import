74a049ae558df2a58cc24ea2056a1b8fe103082d
lipiak <lipiak@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2011-03-26 22:24:08 +0000

    Fixed knobs animation

--- trunk/Orbitersdk/Space Shuttle Ultra/Crawler/LDSPanel.cpp
+++ trunk/Orbitersdk/Space Shuttle Ultra/Crawler/LDSPanel.cpp
@@ -19,7 +19,7 @@ namespace vc
 
 	void LDS::ShowAlignmentError(double lateral)
 	{
-		sprintf(oapiDebugString(),"%lf",lateral);
+		//sprintf(oapiDebugString(),"%lf",lateral);
 	}
 
 	void LDS::CalculateDistanceBetweenAttachments(VECTOR3 MLP_ATTACH_GLOBAL, MATRIX3 RotationMatrix)
@@ -67,10 +67,10 @@ namespace vc
 
 		oapiVCRegisterArea(AID_LDS_AREA+aid_ofs,PANEL_REDRAW_MOUSE,PANEL_MOUSE_LBDOWN);
 		oapiVCSetAreaClickmode_Quadrilateral(AID_LDS_AREA+aid_ofs,
-			TransformVector(_V(-0.416, 0.302, -0.119))+ofs,
-			TransformVector(_V(-0.533, 0.302, 0.023))+ofs, 
-			TransformVector(_V(-0.416, 0.263, -0.119))+ofs,
-			TransformVector(_V(-0.533, 0.263, 0.023))+ofs);
+			TransformVector(_V(-0.451, 0.367, -0.091))+ofs,
+			TransformVector(_V(-0.507, 0.367, 0.002))+ofs, 
+			TransformVector(_V(-0.451, 0.334, -0.091))+ofs,
+			TransformVector(_V(-0.507, 0.334, 0.002))+ofs);
 
 
 	}
@@ -115,22 +115,17 @@ namespace vc
 		
 		//BasicPanel::DefineVCAnimations(mshnbr);
 
-		char lbuf[255];
-		sprintf(lbuf,"%s%f","LDS MESH NUMBER ",vcidx);
-		oapiWriteLog(lbuf);
 
-
-		//THOSE VALUES ARE CORRECT FOR MESH NUMBER 5
 		VECTOR3 LP1,LP2,CP1,CP2,RP1,RP2;
-		LP1 = TransformVector(_V(-0.449,0.286,-0.067));
-		LP2 = TransformVector(_V(-0.409,0.286,-0.036));
-		CP1 = TransformVector(_V(-0.467,0.286,-0.043));
-		CP2 = TransformVector(_V(-0.427,0.286,-0.012));
-		RP1 = TransformVector(_V(-0.486,0.286,-0.020));
-		RP2 = TransformVector(_V(-0.446,0.286,0.011));
+		LP1 = TransformVector(_V(-0.448,0.350,-0.073));
+		LP2 = TransformVector(_V(-0.404,0.350,-0.047));
+		CP1 = TransformVector(_V(-0.463,0.350,-0.047));
+		CP2 = TransformVector(_V(-0.421,0.350,-0.022));
+		RP1 = TransformVector(_V(-0.478,0.350,-0.021));
+		RP2 = TransformVector(_V(-0.434,0.350,0.005));
 
 		//LEFT KNOB ANIMATION
-		static UINT LeftKnobGrp[1] = {57};
+		static UINT LeftKnobGrp[1] = {29};
 		VECTOR3 LResult = _V(LP2.x-LP1.x,LP2.y-LP1.y,LP2.z-LP1.z);
 		VECTOR3 LNormalised = LResult/dist(LP1,LP2);	
 		MGROUP_ROTATE* pRotLeftKnob = new MGROUP_ROTATE(vcidx,LeftKnobGrp,1,LP1,LNormalised,120*RAD); //TRANSFORMED VECTOR
@@ -139,7 +134,7 @@ namespace vc
 		vpAnimations.push_back(pRotLeftKnob);
 
 		//CENTER KNOB ANIMATION
-		static UINT CenterKnobGrp[1] = {58};
+		static UINT CenterKnobGrp[1] = {28};
 		VECTOR3 CResult = _V(CP2.x-CP1.x,CP2.y-CP1.y,CP2.z-CP1.z);
 		VECTOR3 CNormalised = CResult/dist(CP1,CP2);
 		MGROUP_ROTATE* pRotCenterKnob = new MGROUP_ROTATE(vcidx,CenterKnobGrp,1,CP1,CNormalised,120*RAD);
@@ -148,7 +143,7 @@ namespace vc
 		vpAnimations.push_back(pRotCenterKnob);
 
 		//RIGHT KNOB ANIMATION
-		static UINT RightKnobGrp[1] = {59};
+		static UINT RightKnobGrp[1] = {27};
 		VECTOR3 RResult = _V(RP2.x-RP1.x,RP2.y-RP1.y,RP2.z-RP1.z);
 		VECTOR3 RNormalised = RResult/dist(RP1,RP2);
 		MGROUP_ROTATE* pRotRightKnob = new MGROUP_ROTATE(vcidx,RightKnobGrp,1,RP1,RNormalised,120*RAD);
@@ -164,28 +159,28 @@ namespace vc
 
 		if((id == AID_LDS_AREA) || (id==AID_LDS_AREA+AID_REAR_OFFSET))
 		{
-			sprintf(oapiDebugString(),"%f%s%f",p.x," ",p.y);
+			//sprintf(oapiDebugString(),"%f%s%f",p.x," ",p.y);
 
 
-			if(p.x >= 0.270 && p.x < 0.412) //LEFT KNOB
+			if(p.x >= 0 && p.x < 0.26) //LEFT KNOB
 			{
-				if(p.y <= 0.393) //SET LEFT KNOB TO OFF
+				if(p.y <= 0.5) //SET LEFT KNOB TO OFF
 				{
 					V()->SetAnimation(LeftKnobAnim,OFF);
 					LeftKnobState = OFF;
-					sprintf(oapiDebugString(),"%s","LEFT KNOB OFF");
+					//sprintf(oapiDebugString(),"%s","LEFT KNOB OFF");
 				}
 				else //SET KNOB TO ON
 				{	
 					V()->SetAnimation(LeftKnobAnim,ON);
 					LeftKnobState = ON;
-					sprintf(oapiDebugString(),"%s","LEFT KNOB ON");
+					//sprintf(oapiDebugString(),"%s","LEFT KNOB ON");
 				}
 			}
 
-			if(p.x >= 0.412 && p.x < 0.587) //CENTER KNOB
+			if(p.x >= 0.26 && p.x < 0.53) //CENTER KNOB
 			{
-				if(p.y <= 0.393) //SET CENTER KNOB TO OFF
+				if(p.y <= 0.5) //SET CENTER KNOB TO OFF
 				{
 					V()->SetAnimation(CenterKnobAnim,OFF);
 					CenterKnobState = OFF;
@@ -197,9 +192,9 @@ namespace vc
 				}
 			}
 
-			if(p.x >= 0.587 && p.x < 0.737) //RIGHT KNOB
+			if(p.x >= 0.53 && p.x < 1) //RIGHT KNOB
 			{
-				if(p.y <= 0.393) //SET RIGHT KNOB TO OFF
+				if(p.y <= 0.5) //SET RIGHT KNOB TO OFF
 				{
 					V()->SetAnimation(RightKnobAnim,OFF);
 					RightKnobState = OFF;
