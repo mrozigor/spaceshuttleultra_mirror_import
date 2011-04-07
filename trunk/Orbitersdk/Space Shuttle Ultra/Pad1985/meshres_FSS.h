f770219d6a238118d55f266b5116d37bb9a99676
daves-ssu <daves-ssu@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2011-04-07 19:00:55 +0000

    Added project and folder for SSU_Pad1985.

---
+++ trunk/Orbitersdk/Space Shuttle Ultra/Pad1985/meshres_FSS.h
@@ -0,0 +1,65 @@
+// ========================================================
+// Mesh resource file for LC39A_FSS_1985.msh
+// Generated with meshc on Thu Apr 07 18:44:51 2011
+
+// ========================================================
+
+// Number of mesh groups:
+#define NGRP 49
+
+// Number of materials:
+#define NMAT 7
+
+// Number of textures:
+#define NTEX 4
+
+// Named mesh groups:
+#define GRP_Elevator_room 0
+#define GRP_Hinge_columns 1
+#define GRP_GVA_Latch_Back_columns 2
+#define GRP_FSS_levels 3
+#define GRP_Main_FSS_structure 4
+#define GRP_Hammerhead 5
+#define GRP_GH2_fwd_vent_flex_line 6
+#define GRP_GUCP 7
+#define GRP_OTV_Cam_49 8
+#define GRP_OTV_cam_support 9
+#define GRP_GH2_aft_vent_flex_hose 10
+#define GRP_GH2_Pivot_point 11
+#define GRP_GH2_vent_line_haunch 12
+#define GRP_Hammerhead_crane_truss 13
+#define GRP_Intertank_Access_Arm 14
+#define GRP_IAA_extensible_platform 15
+#define GRP_OTV_Cam_03 16
+#define GRP_OTV_Cam_01 17
+#define GRP_RBUS_Platform 18
+#define GRP_RBUS_incline_structure 19
+#define GRP_RBUS_umbilical_beam 20
+#define GRP_GH2_vent_hard_line 21
+#define GRP_GH2_aft_vent_hard_line 22
+#define GRP_OYV_camera_stands 23
+#define GRP_OTV_Cam_61 24
+#define GRP_OTV_Cam_12 25
+#define GRP_OTV_Cam_07 26
+#define GRP_OTV_Cam_08 27
+#define GRP_OTV_Cam_09 28
+#define GRP_OTV_Cam_64 29
+#define GRP_OTV_Cam_63 30
+#define GRP_mast_base 31
+#define GRP_Lightning_mast 32
+#define GRP_Lightning_mast01 33
+#define GRP_RBUS_carrier_plate 34
+#define GRP_Orbiter_Access_Arm 35
+#define GRP_White_Room 36
+#define GRP_North_GOX_vent_cylinder_03 37
+#define GRP_South_GOX_vent_cylinder_01 38
+#define GRP_North_GOX_dockseal 39
+#define GRP_South_GOX_dockseal 40
+#define GRP_GOX_Vent_Hood 41
+#define GRP_South_GOX_vent_cylinder_03 42
+#define GRP_South_GOX_vent_cylinder_02 43
+#define GRP_North_GOX_vent_cylinder_01 44
+#define GRP_North_GOX_vent_cylinder_02 45
+#define GRP_GVA_swing_arm 46
+#define GRP_GVA_swing_arm_fences 47
+#define GRP_GOX_vent_pipes 48
