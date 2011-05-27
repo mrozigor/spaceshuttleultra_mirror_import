a7b872764b29dca68068439e40275579fa54a72d
daves-ssu <daves-ssu@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2011-05-27 23:33:35 +0000

    Checked in updated sources for the pad, in an effort to track down a bug in the Crawler.

--- Orbitersdk/Space Shuttle Ultra/Pad/meshres_FSS_1985.h
+++ Orbitersdk/Space Shuttle Ultra/Pad/meshres_FSS_1985.h
@@ -1,11 +1,11 @@
 // ========================================================
-// Mesh resource file for C:\Orbiter\Orbiter_SSUDev\Meshes\SSU\LC39A_FSS_1985.msh
-// Generated with meshc on Sun Apr 24 21:47:59 2011
+// Mesh resource file for LC39A_FSS_1985.msh
+// Generated with meshc on Thu May 19 17:54:13 2011
 
 // ========================================================
 
 // Number of mesh groups:
-#define NGRP_FSS_1985 50
+#define NGRP_FSS_1985 49
 
 // Number of materials:
 #define NMAT_FSS_1985 7
@@ -15,52 +15,51 @@
 
 // Named mesh groups:
 #define GRP_Elevator_room_FSS_1985 0
-#define GRP_FSS_levels_FSS_1985 1
-#define GRP_GH2_aft_vent_flex_hose_FSS_1985 2
-#define GRP_GH2_aft_vent_hard_line_FSS_1985 3
-#define GRP_GH2_fwd_vent_flex_line_FSS_1985 4
-#define GRP_GH2_Pivot_point_FSS_1985 5
-#define GRP_GH2_vent_hard_line_FSS_1985 6
-#define GRP_GH2_vent_line_haunch_FSS_1985 7
-#define GRP_GUCP_FSS_1985 8
-#define GRP_GVA_Latch_Back_columns_FSS_1985 9
-#define GRP_Hinge_columns_FSS_1985 10
-#define GRP_IAA_extensible_platform_FSS_1985 11
-#define GRP_Intertank_Access_Arm_FSS_1985 12
-#define GRP_Lightning_mast_FSS_1985 13
-#define GRP_Lightning_mast01_FSS_1985 14
-#define GRP_Main_FSS_structure_FSS_1985 15
-#define GRP_OTV_Cam_01_FSS_1985 16
-#define GRP_OTV_Cam_03_FSS_1985 17
-#define GRP_OTV_Cam_07_FSS_1985 18
-#define GRP_OTV_Cam_08_FSS_1985 19
-#define GRP_OTV_Cam_09_FSS_1985 20
-#define GRP_OTV_Cam_12_FSS_1985 21
-#define GRP_OTV_Cam_49_FSS_1985 22
-#define GRP_OTV_Cam_61_FSS_1985 23
-#define GRP_OTV_Cam_63_FSS_1985 24
-#define GRP_OTV_Cam_64_FSS_1985 25
-#define GRP_OTV_cam_support_FSS_1985 26
-#define GRP_OYV_camera_stands_FSS_1985 27
-#define GRP_RBUS_carrier_plate_FSS_1985 28
-#define GRP_RBUS_incline_structure_FSS_1985 29
-#define GRP_RBUS_Platform_FSS_1985 30
-#define GRP_RBUS_umbilical_beam_FSS_1985 31
-#define GRP_GVA_swing_arm_fences_FSS_1985 32
-#define GRP_mast_base_FSS_1985 33
-#define GRP_Hammerhead_crane_truss_FSS_1985 34
-#define GRP_Hammerhead_FSS_1985 35
-#define GRP_GOX_Vent_Hood_FSS_1985 36
-#define GRP_GOX_vent_pipes_FSS_1985 37
-#define GRP_GVA_swing_arm_FSS_1985 38
-#define GRP_North_GOX_dockseal_FSS_1985 39
-#define GRP_North_GOX_vent_cylinder_01_FSS_1985 40
-#define GRP_North_GOX_vent_cylinder_02_FSS_1985 41
-#define GRP_North_GOX_vent_cylinder_03_FSS_1985 42
-#define GRP_South_GOX_dockseal_FSS_1985 43
-#define GRP_South_GOX_vent_cylinder_01_FSS_1985 44
-#define GRP_South_GOX_vent_cylinder_02_FSS_1985 45
-#define GRP_South_GOX_vent_cylinder_03_FSS_1985 46
-#define GRP_White_Room_FSS_1985 47
-#define GRP_Orbiter_Access_Arm_FSS_1985 48
-#define GRP_Box01_FSS_1985 49
+#define GRP_GH2_aft_vent_flex_hose_FSS_1985 1
+#define GRP_GH2_aft_vent_hard_line_FSS_1985 2
+#define GRP_GH2_Pivot_point_FSS_1985 3
+#define GRP_GH2_vent_line_haunch_FSS_1985 4
+#define GRP_GVA_Latch_Back_columns_FSS_1985 5
+#define GRP_Hinge_columns_FSS_1985 6
+#define GRP_IAA_extensible_platform_FSS_1985 7
+#define GRP_Intertank_Access_Arm_FSS_1985 8
+#define GRP_Main_FSS_structure_FSS_1985 9
+#define GRP_OTV_Cam_01_FSS_1985 10
+#define GRP_OTV_Cam_03_FSS_1985 11
+#define GRP_OTV_Cam_07_FSS_1985 12
+#define GRP_OTV_Cam_08_FSS_1985 13
+#define GRP_OTV_Cam_09_FSS_1985 14
+#define GRP_OTV_Cam_12_FSS_1985 15
+#define GRP_OTV_Cam_49_FSS_1985 16
+#define GRP_OTV_Cam_61_FSS_1985 17
+#define GRP_OTV_Cam_63_FSS_1985 18
+#define GRP_OTV_Cam_64_FSS_1985 19
+#define GRP_OTV_cam_support_FSS_1985 20
+#define GRP_OYV_camera_stands_FSS_1985 21
+#define GRP_RBUS_Platform_FSS_1985 22
+#define GRP_White_Room_FSS_1985 23
+#define GRP_Orbiter_Access_Arm_FSS_1985 24
+#define GRP_FSS_levels_FSS_1985 25
+#define GRP_North_GOX_vent_cylinder_02_FSS_1985 26
+#define GRP_South_GOX_vent_cylinder_02_FSS_1985 27
+#define GRP_South_GOX_vent_cylinder_01_FSS_1985 28
+#define GRP_South_GOX_dockseal_FSS_1985 29
+#define GRP_North_GOX_dockseal_FSS_1985 30
+#define GRP_North_GOX_vent_cylinder_01_FSS_1985 31
+#define GRP_South_GOX_vent_cylinder_03_FSS_1985 32
+#define GRP_GVA_swing_arm_FSS_1985 33
+#define GRP_GOX_vent_pipes_FSS_1985 34
+#define GRP_GOX_Vent_Hood_FSS_1985 35
+#define GRP_North_GOX_vent_cylinder_03_FSS_1985 36
+#define GRP_GH2_fwd_vent_flex_line_FSS_1985 37
+#define GRP_GH2_vent_hard_line_FSS_1985 38
+#define GRP_GUCP_FSS_1985 39
+#define GRP_GVA_swing_arm_fences_FSS_1985 40
+#define GRP_RBUS_carrier_plate_FSS_1985 41
+#define GRP_RBUS_umbilical_beam_FSS_1985 42
+#define GRP_RBUS_incline_structure_FSS_1985 43
+#define GRP_Lightning_mast01_FSS_1985 44
+#define GRP_Lightning_mast_FSS_1985 45
+#define GRP_Hammerhead_crane_truss_FSS_1985 46
+#define GRP_Hammerhead_FSS_1985 47
+#define GRP_mlpcolor_FSS_1985 48
