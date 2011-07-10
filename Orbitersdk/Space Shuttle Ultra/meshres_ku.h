0546cbd2a5d1d07222a1f37df241e3dd62b5d82b
daves-ssu <daves-ssu@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2011-07-10 21:49:12 +0000

    Fixed meshgroup names and generated a new meshres header for the Ku antenna.

--- Orbitersdk/Space Shuttle Ultra/meshres_ku.h
+++ Orbitersdk/Space Shuttle Ultra/meshres_ku.h
@@ -1,21 +1,21 @@
 // ========================================================
-// Mesh resource file for ..\..\Meshes\Atlantis\KU.msh
-// Generated with meshc on Tue Feb 05 16:50:33 2008
+// Mesh resource file for KU.msh
+// Generated with meshc on Sun Jul 10 23:48:04 2011
 
 // ========================================================
 
 // Number of mesh groups:
-#define NGRP_KU 5
+#define NGRP 5
 
 // Number of materials:
-#define NMAT_KU 2
+#define NMAT 2
 
 // Number of textures:
-#define NTEX_KU 1
+#define NTEX 1
 
 // Named mesh groups:
-#define GRP_base_KU 0
-#define GRP_cable_one_KU 1
-#define GRP_KUBAND_BOX_KU 2
-#define GRP_KUGIMBAL_KU 3
-#define GRP_KUDISH_KU 4
+#define GRP_GRP_base_KU 0
+#define GRP_GRP_cable_one_KU 1
+#define GRP_GRP_KUBAND_BOX_KU 2
+#define GRP_Group 3
+#define GRP_GRP_KUDISH_KU 4
