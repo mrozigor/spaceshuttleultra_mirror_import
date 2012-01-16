8fc2ffbaaadcce204c3d86273d91edc92c801e1d
siamesecat <siamesecat@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2012-01-16 02:07:49 +0000

    Adding StateVectorPropagator tests based on Orbiter simulation runs.

--- trunk/Orbitersdk/libUltra/libUltraTests/src/StateVectorPropagatorTests.cpp
+++ trunk/Orbitersdk/libUltra/libUltraTests/src/StateVectorPropagatorTests.cpp
@@ -1,7 +1,7 @@
 #include "StateVectorPropagator.h"
 #include "gtest\gtest.h"
 
-TEST(StateVectorPropagatorTest, PropagatesStateVector) {
+TEST(StateVectorPropagatorTest, OctavePropagation) {
 	StateVectorPropagator propagator(0.2, 50, 4000.0);
 	propagator.SetParameters(100.0, 5.9742e24, 6371000.0, 0.00108263);
 	//propagator.UpdateStateVector(_V(6571000.0, 0.0, 0.0), _V(5444.72, 5444.72, 0.0), 0.0);
@@ -20,6 +20,53 @@ TEST(StateVectorPropagatorTest, PropagatesStateVector) {
 	EXPECT_NEAR(vel.x, 7.0350e3, 10.0);
 	EXPECT_NEAR(vel.y, 4.1099e3, 10.0);
 	EXPECT_NEAR(vel.z, 8.6403e2, 10.0);
+}
+
+TEST(StateVectorPropagatorTest, OrbiterPropagation10x) {
+	StateVectorPropagator propagator(0.2, 50, 4000.0);
+	propagator.SetParameters(96904.650960, 5973698968000000000000000.0, 6371010.0, 0.001083);
+	propagator.UpdateStateVector(_V(5056798.528649, -547320.350203, -4271363.579233), _V(4602.657749, 3643.030605, 5068.732737), 268.665);
+	double met = 268.665;
+	for(int i=0;i<10000;i++) {
+		met+=0.05; // 20 fps framerate
+		propagator.Step(met, 0.05);
+	}
+	// compare to 'correct' results from Orbiter (10x time acceleration)
+	VECTOR3 pos, vel;
+	/*propagator.GetStateVectors(2403.979, pos, vel);
+	EXPECT_NEAR(pos.x, -1758969.066978, 50.0);
+	EXPECT_NEAR(pos.y, 2255303.794633, 50.0);
+	EXPECT_NEAR(pos.z, 5975651.945430, 50.0);
+	EXPECT_NEAR(vel.x, -7252.797987, 2.0);
+	EXPECT_NEAR(vel.y, -2569.911904, 2.0);
+	EXPECT_NEAR(vel.z, -1109.049837, 2.0);*/
+	propagator.GetStateVectors(3490.362, pos, vel);
+	EXPECT_NEAR(pos.x, -6467033.167073, 100.0);
+	EXPECT_NEAR(pos.y, -1433782.906445, 10.0);
+	EXPECT_NEAR(pos.z, 876563.970814, 10.0);
+	EXPECT_NEAR(vel.x, -257.260709, 2.0);
+	EXPECT_NEAR(vel.y, -3285.919386, 2.0);
+	EXPECT_NEAR(vel.z, -6968.483088, 2.0);
+}
+
+TEST(StateVectorPropagatorTest, OrbiterPropagation1x) {
+	StateVectorPropagator propagator(0.2, 50, 4000.0);
+	propagator.SetParameters(96904.650960, 5973698968000000000000000.0, 6371010.0, 0.001083);
+	propagator.UpdateStateVector(_V(4122635.433053, -1186636.293658, -5083779.558075), _V(5730.532653, 3429.424923, 3919.439554), 88.537000);
+	double met = 88.537000;
+	for(int i=0;i<10000;i++) {
+		met+=0.05; // 20 fps framerate
+		propagator.Step(met, 0.05);
+	}
+	// compare to 'correct' results from Orbiter (no time acceleration)
+	VECTOR3 pos, vel;
+	propagator.GetStateVectors(2740.014, pos, vel);
+	EXPECT_NEAR(pos.x, -3999802.109840, 50.0);
+	EXPECT_NEAR(pos.y, 1241300.201609, 50.0);
+	EXPECT_NEAR(pos.z, 5156717.506392, 50.0);
+	EXPECT_NEAR(vel.x, -5913.843901, 2.0);
+	EXPECT_NEAR(vel.y, -3386.079311, 2.0);
+	EXPECT_NEAR(vel.z, -3698.921817, 2.0);
 }
 
 TEST(StateVectorPropagatorTest, CalculatesApogeeAndPerigee) {
