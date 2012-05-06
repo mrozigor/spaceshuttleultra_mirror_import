0a5551a9cdcc83265116e933ae2eaa9b075e634c
daves-ssu <daves-ssu@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2012-05-06 23:02:13 +0000

    Updated meshes and textures for the orbiter, OBSS, starboard/port MPMs, the SRMS and Ku band DA. Need someone to update the meshres files for the starboard MPMS and the SRMS.

--- Orbitersdk/Space Shuttle Ultra/meshres.h
+++ Orbitersdk/Space Shuttle Ultra/meshres.h
@@ -1,22 +1,20 @@
 // ========================================================
-// MESH RESOURCE FILE FOR ..\..\MESHES\SSU\ORBITER.MSH
-// GENERATED WITH MESHC ON THU MAR 19 22:28:06 2009
+// Mesh resource file for Orbiter.msh
+// Generated with meshc on Thu May 03 13:26:31 2012
 
 // ========================================================
 
-#pragma once
-
-// NUMBER OF MESH GROUPS:
+// Number of mesh groups:
 #define NGRP 74
 
-// NUMBER OF MATERIALS:
+// Number of materials:
 #define NMAT 8
 
-// NUMBER OF TEXTURES:
+// Number of textures:
 #define NTEX 2
 
-// NAMED MESH GROUPS:
-#define GRP_OBJECT 0
+// Named mesh groups:
+#define GRP_FUSELAGE 0
 #define GRP_BODYFLAP 1
 #define GRP_CARGODOOROUTR 2
 #define GRP_CARGODOOROUTL 3
@@ -46,47 +44,47 @@
 #define GRP_RUDDERL 27
 #define GRP_PROBEL 28
 #define GRP_PROBER 29
-#define GRP_CARGOBAY 30
-#define GRP_CARGODOORINR 31
-#define GRP_CARGODOORINL 32
-#define GRP_KUBAND1 33
-#define GRP_KUBAND2 34
-#define GRP_PANTILT_FL 35
-#define GRP_CCTV_FL 36
-#define GRP_CCTV_BL 37
-#define GRP_PANTILT_BL 38
-#define GRP_CCTV_FR 39
-#define GRP_PANTILT_FR 40
-#define GRP_PANTILT_BR 41
-#define GRP_CCTV_BR 42
-#define GRP_RADIATORFL 43
-#define GRP_RADIATORFR 44
-#define GRP_RADIATORBR 45
-#define GRP_RADIATORBL 46
-#define GRP_STARTRACKERS 47
-#define GRP_TRACKERPLATES 48
-#define GRP_Z_STARTRACKDOOR 49
-#define GRP_Y_STARTRACKDOOR 50
-#define GRP_LEFTFLAPPERIN 51
-#define GRP_LEFTFLAPPEROUT 52
-#define GRP_RIGHTFLAPPERIN 53
-#define GRP_RIGHTFLAPPEROUT 54
-#define GRP_PORTPUSH_ROD 55
-#define GRP_PORT_CLAMP 56
-#define GRP_PORTDOOR_PULL 57
-#define GRP_STBDPUSH_ROD 58
-#define GRP_STBD_CLAMP 59
-#define GRP_STBDDOOR_PULL 60
-#define GRP_AFT_HOOKS 61
-#define GRP_MID_AFT_HOOKS 62
-#define GRP_MID_FWD_HOOKS 63
-#define GRP_FWD_HOOKS 64
-#define GRP_HUDL 65
-#define GRP_HUDR 66
-#define GRP_WINDOWS 67
-#define GRP_Port_VentDoor3 68
-#define GRP_Port_VentDoor5 69
-#define GRP_Port_VentDoor6 70
-#define GRP_STBD_VentDoor3 71
-#define GRP_STBD_VentDoor5 72
-#define GRP_STBD_VentDoor6 73
+#define GRP_CARGODOORINR 30
+#define GRP_CARGODOORINL 31
+#define GRP_KUBAND1 32
+#define GRP_KUBAND2 33
+#define GRP_PANTILT_FL 34
+#define GRP_CCTV_FL 35
+#define GRP_CCTV_BL 36
+#define GRP_PANTILT_BL 37
+#define GRP_CCTV_FR 38
+#define GRP_PANTILT_FR 39
+#define GRP_PANTILT_BR 40
+#define GRP_CCTV_BR 41
+#define GRP_RADIATORFL 42
+#define GRP_RADIATORFR 43
+#define GRP_RADIATORBR 44
+#define GRP_RADIATORBL 45
+#define GRP_STARTRACKERS 46
+#define GRP_TRACKERPLATES 47
+#define GRP_Z_STARTRACKDOOR 48
+#define GRP_Y_STARTRACKDOOR 49
+#define GRP_LEFTFLAPPERIN 50
+#define GRP_LEFTFLAPPEROUT 51
+#define GRP_RIGHTFLAPPERIN 52
+#define GRP_RIGHTFLAPPEROUT 53
+#define GRP_PORTPUSH_ROD 54
+#define GRP_PORT_CLAMP 55
+#define GRP_PORTDOOR_PULL 56
+#define GRP_STBDPUSH_ROD 57
+#define GRP_STBD_CLAMP 58
+#define GRP_STBDDOOR_PULL 59
+#define GRP_AFT_HOOKS 60
+#define GRP_MID_AFT_HOOKS 61
+#define GRP_MID_FWD_HOOKS 62
+#define GRP_FWD_HOOKS 63
+#define GRP_STBDDOOR3 64
+#define GRP_STBDDOOR5 65
+#define GRP_STBDDOOR7 66
+#define GRP_PORTDOOR3 67
+#define GRP_PORTDOOR5 68
+#define GRP_PORTDOOR7 69
+#define GRP_HUDL 71
+#define GRP_HUDR 72
+#define GRP_WINDOWS 73
+#define GRP_CARGOBAY 73
