72b96081a8796bdded9f240cde29fb43f7bc0ce5
daves-ssu <daves-ssu@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2012-09-17 18:21:41 +0000

    Updated the animations for the Ku band DA. Also deleted the old unused KU.msh.

--- trunk/Orbitersdk/Space Shuttle Ultra/meshres_ku.h
+++ trunk/Orbitersdk/Space Shuttle Ultra/meshres_ku.h
@@ -1,11 +1,11 @@
 // ========================================================
 // Mesh resource file for Ku_band_DA.msh
-// Generated with meshc on Tue Aug 28 22:14:50 2012
+// Generated with meshc on Mon Sep 17 20:14:14 2012
 
 // ========================================================
 
 // Number of mesh groups:
-#define NGRP_KU 9
+#define NGRP_KU 7
 
 // Number of materials:
 #define NMAT_KU 2
@@ -16,10 +16,8 @@
 // Named mesh groups:
 #define GRP_DEA_KU 0
 #define GRP_Alpha_gimbal_KU 1
-#define GRP_Dish_KU 2
-#define GRP_Dish_receiver_supports_KU 3
-#define GRP_Hinge_connectors_KU 4
-#define GRP_DEA_cable_static_KU 5
-#define GRP_DEA_cable_rotating_KU 6
-#define GRP_Gimbal_lock_structure_KU 7
-#define GRP_Beta_gimbal_boom_KU 8
+#define GRP_Beta_gimbal_boom_KU 2
+#define GRP_Gimbal_lock_structure_KU 3
+#define GRP_Dish_receiver_supports_KU 4
+#define GRP_Dish_KU 5
+#define GRP_DEA_cable_static_KU 6
