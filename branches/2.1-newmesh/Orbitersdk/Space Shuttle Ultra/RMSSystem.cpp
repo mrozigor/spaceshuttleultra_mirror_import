33253d13ae5f0cf43886d57209e6d09652b242f8
siamesecat <siamesecat@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2014-01-08 02:04:04 +0000

    Correcting RMS elbow camera animation and camera position.

--- branches/2.1-newmesh/Orbitersdk/Space Shuttle Ultra/RMSSystem.cpp
+++ branches/2.1-newmesh/Orbitersdk/Space Shuttle Ultra/RMSSystem.cpp
@@ -188,12 +188,12 @@ void RMSSystem::CreateArm()
 	//RMS elbow camera
 	static UINT RMSElbowCamGrp[2] = {GRP_ELBOWCAM, GRP_CAMSWIVEL};
 	MGROUP_ROTATE* pRMSElbowCamPan = new MGROUP_ROTATE(mesh_index, RMSElbowCamGrp+1, 1,
-		_V(-2.68, 0.12, 0.07), _V(0.149537, -0.988756, 0), (float)(340*RAD));
+		_V(-2.68, 0.12, 0.07), _V(0.124034734589, 0.992277876713, 0.0), (float)(340*RAD));
 	ANIMATIONCOMPONENT_HANDLE parent2;
 	anim_camRMSElbow[PAN]=STS()->CreateAnimation(0.5);
 	parent2 = STS()->AddManagedAnimationComponent (anim_camRMSElbow[PAN], 0, 1, pRMSElbowCamPan, parent);
 	MGROUP_ROTATE* pRMSElbowCamTilt = new MGROUP_ROTATE(mesh_index, RMSElbowCamGrp, 1,
-		_V(-2.65, 0.34, 0.07), _V(0.990268, 0.139173, 0), (float)(340*RAD));
+		_V(-2.65, 0.34, 0.07), _V(0.992277876713, -0.124034734589, 0.0), (float)(340*RAD));
 	anim_camRMSElbow[TILT]=STS()->CreateAnimation(0.5);
 	parent2 = STS()->AddManagedAnimationComponent(anim_camRMSElbow[TILT], 0, 1, pRMSElbowCamTilt, parent2);
 	MGROUP_ROTATE* pRMSElbowCamLoc = new MGROUP_ROTATE(LOCALVERTEXLIST, MAKEGROUPARRAY(camRMSElbowLoc), 2,
@@ -943,7 +943,7 @@ void RMSSystem::UpdateElbowCamView() const
 {
 	if(oapiCameraInternal()) {
 		STS()->SetCameraDefaultDirection(camRMSElbowLoc[1]-camRMSElbowLoc[0]);
-		STS()->SetCameraOffset(camRMSElbowLoc[0]+RMS_MESH_OFFSET);
+		STS()->SetCameraOffset(STS()->GetOrbiterCoGOffset()+camRMSElbowLoc[0]+RMS_MESH_OFFSET);
 		oapiCameraSetCockpitDir(0.0, 0.0);
 	}
 }
