3de93a0bd88477937b997f585838584b447cee83
orbinautgoth <orbinautgoth@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2014-03-16 21:54:31 +0000

    First prototype of .NET version of SSUMissionControl and SSUToolbox

---
+++ branches/SSUToolbox/Orbitersdk/Space Shuttle Ultra/SSUToolbox/MissionControlDLL/MissionControlDLL.cpp
@@ -0,0 +1,199 @@
+//code by Face
+
+// ==============================================================
+//                 ORBITER MODULE: DialogTemplate
+//                  Part of the ORBITER SDK
+//            Copyright (C) 2003 Martin Schweiger
+//                   All rights reserved
+//
+// DialogTemplate.cpp
+//
+// This module demonstrates how to build an Orbiter plugin which
+// opens a Windows dialog box. This is a good starting point for
+// your own dialog-based addons.
+// ==============================================================
+#define STRICT
+#define ORBITER_MODULE
+
+#include <vcclr.h>
+#include "Launcher.h"
+
+#pragma unmanaged
+
+#include "orbitersdk.h"
+
+#include "windows.h"
+#include "resource.h"
+
+// ==============================================================
+// Global variables
+// ==============================================================
+
+HINSTANCE g_hInst;  // module instance handle
+DWORD g_dwCmd;      // custom function identifier
+
+// ==============================================================
+// Local prototypes
+// ==============================================================
+
+void OpenDlgClbk (void *context);
+BOOL CALLBACK MsgProc (HWND, UINT, WPARAM, LPARAM);
+
+#pragma managed
+
+using namespace SSU::Toolbox::Client;
+using namespace System;
+using namespace System::IO;
+using namespace System::Text;
+using namespace System::Reflection;
+using namespace System::Runtime::InteropServices;
+
+Wrapper wrapper(gcnew Launcher());
+
+namespace SSU
+{
+	namespace Toolbox
+	{
+		namespace Client
+		{
+			void CreateUnmanagedDialog (Wrapper *wrapper, HWND managed){wrapper->CreateUnmanagedDialog(managed);}
+			void CloseUnmanagedDialog (Wrapper *wrapper, HWND managed){wrapper->CloseUnmanagedDialog(managed);}
+
+			Launcher::Launcher()
+			{				
+				String^ pathONav = 
+					Path::GetDirectoryName(Assembly::GetExecutingAssembly()->Location);
+				wchar_t backslash = Path::DirectorySeparatorChar;
+				int test = pathONav->LastIndexOf(Path::DirectorySeparatorChar, pathONav->LastIndexOf(Path::DirectorySeparatorChar)-1);
+				pathONav = pathONav->Remove(pathONav->LastIndexOf(Path::DirectorySeparatorChar, pathONav->LastIndexOf(Path::DirectorySeparatorChar)-1));
+				
+				pathONav = Path::Combine(pathONav, "SSUToolbox\\SSUToolbox.exe");
+
+
+				Assembly^ assm=Assembly::LoadFrom(pathONav);
+				if (assm!=nullptr)
+				{
+					for each (Type^ t in assm->GetTypes())
+					{
+						if (t->Name->EndsWith("Program"))
+						{
+							t->GetProperty("CreateDialogFunction")->SetValue(nullptr, (IntPtr)CreateUnmanagedDialog, nullptr);
+							t->GetProperty("CloseDialogFunction")->SetValue(nullptr, (IntPtr)CloseUnmanagedDialog, nullptr);
+							wrapper=t->GetProperty("Wrapper");
+							open=t->GetMethod("Open");
+							init=t->GetMethod("Init");
+							break;
+						}
+					}
+				}				
+			}
+			void Launcher::SetWrapper(Wrapper *wrapper)
+			{
+				this->wrapper->SetValue(nullptr, (System::IntPtr)wrapper, nullptr);
+			}
+			void Launcher::Open(){open->Invoke(nullptr, nullptr);}
+			void Launcher::Init(){init->Invoke(nullptr, nullptr);}
+
+			Wrapper::Wrapper(Launcher ^launcher)
+			{
+				current=NULL;
+				this->launcher=launcher;
+				launcher->SetWrapper(this);
+			}
+			HWND Wrapper::GetManaged(HWND unmanaged){return current==NULL?unmanaged2managed[unmanaged]:current;}
+			void Wrapper::CreateUnmanagedDialog(HWND managed)
+			{
+				current=managed;
+				HWND unmanaged=oapiOpenDialogEx(g_hInst, IDD_EMPTY, MsgProc, DLG_ALLOWMULTI);
+				unmanaged2managed[unmanaged]=managed;
+				managed2unmanaged[managed]=unmanaged;
+				current=NULL;
+			}
+			void Wrapper::CloseUnmanagedDialog(HWND managed)
+			{
+				oapiCloseDialog(managed2unmanaged[managed]);
+				unmanaged2managed.erase(managed2unmanaged[managed]);
+				managed2unmanaged.erase(managed);
+			}
+			void Wrapper::Open(){launcher->Open();}
+			void Wrapper::Init(){launcher->Init();}
+		}
+	}
+}
+
+#pragma unmanaged
+
+// ==============================================================
+// API interface
+// ==============================================================
+
+// ==============================================================
+// This function is called when Orbiter starts or when the module
+// is activated.
+
+DLLCLBK void InitModule (HINSTANCE hDLL)
+{
+	g_hInst = hDLL; // remember the instance handle
+	g_dwCmd = oapiRegisterCustomCmd ("SSU Mission Control",
+		"SSU Mission Control.\r\nMission Control for Space Shuttle Ultra",
+		OpenDlgClbk, NULL);
+}
+
+// ==============================================================
+// This function is called when Orbiter shuts down or when the
+// module is deactivated
+
+DLLCLBK void ExitModule (HINSTANCE hDLL)
+{
+	//oapiUnregisterCustomCmd (g_dwCmd);	
+}
+
+void OpenDlgClbk (void *context)
+{	
+	wrapper.Open();
+}
+
+DLLCLBK void opcOpenRenderViewport (HWND hRenderWnd, DWORD width, DWORD height, BOOL fullscreen)
+{
+	wrapper.Init();
+}
+
+// ==============================================================
+// Windows message handler for the dialog box
+BOOL CALLBACK MsgProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{		
+	LONG style;
+	HWND hManaged;
+	RECT rManaged;
+	static char title[1025];
+	
+	switch (uMsg)
+	{
+	case WM_SHOWWINDOW:		
+		if (wParam==TRUE)
+		{
+			hManaged=wrapper.GetManaged(hDlg);
+			style=GetWindowLong(hManaged, GWL_STYLE);
+			SetWindowLong(hManaged, GWL_STYLE, style | WS_CHILD);
+			GetWindowRect(hManaged, &rManaged);
+			AdjustWindowRect(&rManaged, WS_POPUPWINDOW | WS_CAPTION, false);
+			SetParent(hManaged, hDlg);
+			SetWindowPos(hDlg, NULL, rManaged.left, rManaged.top, rManaged.right-rManaged.left, rManaged.bottom-rManaged.top, NULL);
+			GetWindowText(hManaged, (LPWSTR)title, 1024);
+			SetWindowText(hDlg, (LPWSTR)title);
+			ShowWindow(hManaged, SW_SHOW);
+			GetClientRect(hDlg, &rManaged);
+			SendMessage(hManaged, WM_SIZING, WMSZ_BOTTOMRIGHT, (LPARAM)&rManaged);
+		}		
+		break;		
+	case WM_SIZING:
+		hManaged=wrapper.GetManaged(hDlg);
+		GetClientRect(hDlg, &rManaged);
+		SendMessage(hManaged, uMsg, wParam, (LPARAM)&rManaged);
+		break;
+	case WM_CLOSE:
+	case WM_ACTIVATE:
+		SendMessage(wrapper.GetManaged(hDlg), uMsg, wParam, lParam);
+	}
+	return oapiDefDialogProc (hDlg, uMsg, wParam, lParam);
+}
---
+++ branches/SSUToolbox/Orbitersdk/Space Shuttle Ultra/SSUToolbox/SSUToolbox/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+//using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace SSUToolbox
+{
+    public delegate void ModalResultDelegate(DialogResult result);
+    static class Program
+    {
+        private static Form mainForm;
+        public static bool standalone = true;
+
+        private static TextWriterTraceListener PreRun()
+        {
+            TextWriterTraceListener traceListener = null;
+            try
+            {
+                FileStream traceFile = new FileStream("SSUToolbox.log", FileMode.Create);
+                traceListener = new TextWriterTraceListener(traceFile);
+                traceListener.TraceOutputOptions = TraceOptions.DateTime;
+
+                Trace.Listeners.Add(traceListener);
+            }
+            catch (IOException)
+            {
+            }
+
+            return traceListener;
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            TextWriterTraceListener traceListener = PreRun();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(mainForm = new Form());
+
+            PostRun(traceListener);
+        }
+
+        private static void PostRun(TextWriterTraceListener traceListener)
+        {
+            //Controller.exit();            //???
+
+            if (traceListener != null)
+            {
+                traceListener.Flush();
+                traceListener.Close();
+            }
+        }
+
+        public static void Open()
+        {
+            CreateDialog(mainForm);
+        }
+
+        public static void Init()
+        {
+            standalone = false;
+            mainForm = new Form();
+            TextWriterTraceListener traceListener = PreRun();
+            mainForm.Closed += delegate { PostRun(traceListener); };
+        }
+
+        private delegate void CreateDialogDelegate(IntPtr wrapper, IntPtr handle);
+
+        private static CreateDialogDelegate createDialog;
+        private static CreateDialogDelegate closeDialog;
+
+        public static IntPtr CreateDialogFunction
+        {
+            set { createDialog = (CreateDialogDelegate)Marshal.GetDelegateForFunctionPointer(value, typeof(CreateDialogDelegate)); }
+        }
+
+        public static IntPtr CloseDialogFunction
+        {
+            set { closeDialog = (CreateDialogDelegate)Marshal.GetDelegateForFunctionPointer(value, typeof(CreateDialogDelegate)); }
+        }
+
+        public static void CreateDialog(Form form)
+        {
+            if (createDialog != null && closeDialog != null)
+            {
+                form.Closed += (sender, e) =>
+                {
+                    closeDialog(Wrapper, form.Handle);
+                    form.Dispose();
+                };
+                CreateUnmanagedDialog(form);
+            }
+            else form.Show();
+        }
+
+        private static void CreateUnmanagedDialog(Form form)
+        {
+            form.FormBorderStyle = FormBorderStyle.None;
+            mainForm.Focus();
+            createDialog(Wrapper, form.Handle);
+            form.Location = new Point(0, 0);
+        }
+
+        public static void CreateDialogModal(Form form, ModalResultDelegate modalFunction)
+        {
+            if (createDialog != null && closeDialog != null)
+            {
+                mainForm.Enabled = false;
+                form.Closed += (sender, e) =>
+                {
+                    mainForm.Enabled = true;
+                    closeDialog(Wrapper, form.Handle);
+                    modalFunction.BeginInvoke(form.DialogResult, null, null);
+                    form.Dispose();
+                };
+                CreateUnmanagedDialog(form);
+            }
+            else modalFunction(form.ShowDialog());
+        }
+
+        public static void CreateDialogModal(CommonDialog form, ModalResultDelegate modalFunction)
+        {
+            if (createDialog != null && closeDialog != null)
+            {
+                mainForm.Enabled = false;
+                Thread dialog = new Thread(new ThreadStart(delegate
+                {
+                    var result = form.ShowDialog();
+                    mainForm.Invoke(new MethodInvoker(delegate { mainForm.Enabled = true; }));
+                    modalFunction.BeginInvoke(result, null, null);
+                }));
+                dialog.SetApartmentState(ApartmentState.STA);
+                dialog.Start();
+            }
+            else modalFunction(form.ShowDialog());
+        }
+
+        public static IntPtr Wrapper { get; set; }
+    }
+}
