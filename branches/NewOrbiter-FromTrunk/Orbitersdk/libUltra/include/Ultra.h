7c36d34a7c06eb3531b24dfd27ca22e6c2c8e56b
siamesecat <siamesecat@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2014-12-14 20:09:01 +0000

    Closing NewOrbiter-FromTrunk branch (branch has been merged into trunk).

--- branches/NewOrbiter-FromTrunk/Orbitersdk/libUltra/include/Ultra.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-#include "Mission/Mission.h"
-
-bool __cdecl ssuInit();
-bool __cdecl ssuExit();
-extern "C++" mission::Mission*  __cdecl ssuGetMission(const std::string& filename);
