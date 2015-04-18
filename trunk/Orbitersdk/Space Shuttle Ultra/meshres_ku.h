e1593de9d239135ea5adae0286a54360a7ce7769
daves-ssu <daves-ssu@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2015-04-18 01:29:32 +0000

    Checked in the updated meshres headers for the orbiter and Ku band DA

--- trunk/Orbitersdk/Space Shuttle Ultra/meshres_ku.h
+++ trunk/Orbitersdk/Space Shuttle Ultra/meshres_ku.h
@@ -1,6 +1,6 @@
 // ======================================================
 // Created by ssumeshc 0.2
-// Date of conversion : Wed Mar 25 17:07:31 2015
+// Date of conversion : Thu Apr 16 21:21:14 2015
 
 // Input file: Ku_band_DA.msh
 // ======================================================
@@ -9,14 +9,14 @@
 
 const unsigned int NUMGRP_KU = 10;
 
-const unsigned int GRP_BETA_GIMBAL_BOOM_KU	=	0;	//Beta_gimbal_boom
-const unsigned int GRP_ALPHA_GIMBAL_KU	=	1;	//Alpha_gimbal
-const unsigned int GRP_DEA_KU	=	2;	//DEA
-const unsigned int GRP_DEA_EQUIPMENT_KU	=	3;	//DEA_EQUIPMENT
-const unsigned int GRP_DEA_CABLE_STATIC_KU	=	4;	//DEA_cable_static
-const unsigned int GRP_DEAA_KU	=	5;	//DEA
-const unsigned int GRP_DISH_RECEIVER_SUPPORTS_KU	=	6;	//Dish_receiver_supports
-const unsigned int GRP_DISH_KU	=	7;	//Dish
-const unsigned int GRP_GIMBAL_LOCK_STRUCTURE_KU	=	8;	//Gimbal_lock_structure
-const unsigned int GRP_DEAB_KU	=	9;	//DEA
+const unsigned int GRP_DEA_MOUNT_KU	=	0;	//DEA_MOUNT
+const unsigned int GRP_DEA_EQUIPMENT_KU	=	1;	//DEA_EQUIPMENT
+const unsigned int GRP_DEA_STATIC_CABLE_KU	=	2;	//DEA_STATIC_CABLE
+const unsigned int GRP_BETA_GIMBAL_BOOM_KU	=	3;	//Beta_gimbal_boom
+const unsigned int GRP_DISH_KU	=	4;	//Dish
+const unsigned int GRP_GIMBAL_LOCK_STRUCTURE_KU	=	5;	//Gimbal_lock_structure
+const unsigned int GRP_DEA_KU	=	6;	//DEA
+const unsigned int GRP_ALPHA_GIMBAL_KU	=	7;	//Alpha_gimbal
+const unsigned int GRP_DEA_ACTUATOR_KU	=	8;	//DEA_ACTUATOR
+const unsigned int GRP_DISH_RECEIVER_SUPPORTS_KU	=	9;	//Dish_receiver_supports
 
