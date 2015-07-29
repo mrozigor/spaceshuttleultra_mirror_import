188e5e3baef8ec72bb48250d6b4843f7ea1db69c
gls_ssu <gls_ssu@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2015-07-29 12:26:44 +0000

    added spring loaded capability to LockableLever class

--- branches/ius-1.0/Orbitersdk/Space Shuttle Ultra/vc/PanelL10.cpp
+++ branches/ius-1.0/Orbitersdk/Space Shuttle Ultra/vc/PanelL10.cpp
@@ -639,7 +639,7 @@ namespace vc
 		pTiltTableActuatorAltDrAct1->SetReference( _V( -1.1769, 2.2007, 13.5538 ), SWITCH_ROT );
 		pTiltTableActuatorAltDrAct1->SetMouseRegion( 0.754145f, 0.717768f, 0.787739f, 0.759606f );
 		pTiltTableActuatorAltDrAct1->SetPullDirection( SWITCH_PULL );
-		pTiltTableActuatorAltDrAct1->SetSpringLoaded( true, 1 );// does not work
+		pTiltTableActuatorAltDrAct1->SetSpringLoaded( true, 1 );
 
 		pCmdPathEnabled->SetInitialAnimState( 0.5f );
 		pCmdPathEnabled->DefineSwitchGroup( GRP_L10TOGGLE3_VC );
@@ -680,14 +680,14 @@ namespace vc
 		pUmbilicalsRelPri->SetReference( _V( -1.0922, 2.1459, 13.3103 ), SWITCH_ROT );
 		pUmbilicalsRelPri->SetMouseRegion( 0.395237f, 0.887448f, 0.424710f, 0.930452f );
 		pUmbilicalsRelPri->SetPullDirection( SWITCH_PULL );
-		pUmbilicalsRelPri->SetSpringLoaded( true, 1 );// does not work
+		pUmbilicalsRelPri->SetSpringLoaded( true, 1 );
 
 		pUmbilicalsRelAlt->SetInitialAnimState( 0.5f );
 		pUmbilicalsRelAlt->DefineSwitchGroup( GRP_L10TOGGLE5_VC );
 		pUmbilicalsRelAlt->SetReference( _V( -1.0922, 2.1459, 13.3103 ), SWITCH_ROT );
 		pUmbilicalsRelAlt->SetMouseRegion( 0.449887f, 0.889443f, 0.480940f, 0.931571f );
 		pUmbilicalsRelAlt->SetPullDirection( SWITCH_PULL );
-		pUmbilicalsRelAlt->SetSpringLoaded( true, 1 );// does not work
+		pUmbilicalsRelAlt->SetSpringLoaded( true, 1 );
 
 		pIUSDeploymentEnaPri->SetInitialAnimState( 0.5f );
 		pIUSDeploymentEnaPri->DefineSwitchGroup( GRP_L10SWITCH43_VC );
@@ -708,21 +708,21 @@ namespace vc
 		pIUSDeploymentDpyPri->SetReference( _V( -1.0922, 2.1459, 13.3103 ), SWITCH_ROT );
 		pIUSDeploymentDpyPri->SetMouseRegion( 0.666605f, 0.889142f, 0.699841f, 0.929472f );
 		pIUSDeploymentDpyPri->SetPullDirection( SWITCH_PULL );
-		pIUSDeploymentDpyPri->SetSpringLoaded( true, 1 );// does not work
+		pIUSDeploymentDpyPri->SetSpringLoaded( true, 1 );
 
 		pIUSDeploymentDpyAlt->SetInitialAnimState( 0.5f );
 		pIUSDeploymentDpyAlt->DefineSwitchGroup( GRP_L10TOGGLE7_VC );
 		pIUSDeploymentDpyAlt->SetReference( _V( -1.0922, 2.1459, 13.3103 ), SWITCH_ROT );
 		pIUSDeploymentDpyAlt->SetMouseRegion( 0.726243f, 0.889508f, 0.758000f, 0.929481f );
 		pIUSDeploymentDpyAlt->SetPullDirection( SWITCH_PULL );
-		pIUSDeploymentDpyAlt->SetSpringLoaded( true, 1 );// does not work
+		pIUSDeploymentDpyAlt->SetSpringLoaded( true, 1 );
 
 		pPowerKill->SetInitialAnimState( 0.5f );
 		pPowerKill->DefineSwitchGroup( GRP_L10TOGGLE8_VC );
 		pPowerKill->SetReference( _V( -1.1144, 2.1571, 13.5453 ), SWITCH_ROT );
 		pPowerKill->SetMouseRegion( 0.834209f, 0.848043f, 0.869189f, 0.889788f );
 		pPowerKill->SetPullDirection( SWITCH_PULL );
-		pPowerKill->SetSpringLoaded( true, 1 );// does not work
+		pPowerKill->SetSpringLoaded( true, 1 );
 
 		pPowerSourceASEBatteriesPriTB->DefineMeshGroup( mesh_index, GRP_L10TB1_VC );
 		pPowerSourceASEBatteriesAltTB->DefineMeshGroup( mesh_index, GRP_L10TB2_VC );
