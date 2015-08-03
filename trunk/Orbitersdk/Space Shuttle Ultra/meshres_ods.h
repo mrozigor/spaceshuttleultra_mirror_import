c42bb72a3016841bd471446a8c8f648926551f50
daves-ssu <daves-ssu@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2015-08-03 19:59:54 +0000

    Added the missing 10 cm to the ODS docking ring rods

--- trunk/Orbitersdk/Space Shuttle Ultra/meshres_ods.h
+++ trunk/Orbitersdk/Space Shuttle Ultra/meshres_ods.h
@@ -1,48 +1,49 @@
 // ======================================================
 // Created by ssumeshc 0.2
-// Date of conversion : Sat Feb 21 17:17:23 2015
+// Date of conversion : Mon Aug 03 21:55:50 2015
 
 // Input file: ODS.msh
 // ======================================================
 
 #pragma once
 
-const unsigned int NUMGRP_ODS = 36;
+const unsigned int NUMGRP_ODS = 37;
 
-const unsigned int GRP_TUBE01_ODS	=	0;	//Tube01
-const unsigned int GRP_BOX65_ODS	=	1;	//Box65
-const unsigned int GRP_TUBE02_ODS	=	2;	//Tube02
-const unsigned int GRP_TUBE04_ODS	=	3;	//Tube04
-const unsigned int GRP_CYLINDER12_ODS	=	4;	//Cylinder12
-const unsigned int GRP_CYLINDER13_ODS	=	5;	//Cylinder13
-const unsigned int GRP_CYLINDER17_ODS	=	6;	//Cylinder17
-const unsigned int GRP_BOX21_ODS	=	7;	//Box21
-const unsigned int GRP_BOX15_ODS	=	8;	//Box15
-const unsigned int GRP_BOX14_ODS	=	9;	//Box14
-const unsigned int GRP_BOX69_ODS	=	10;	//Box69
-const unsigned int GRP_BOX70_ODS	=	11;	//Box70
-const unsigned int GRP_CYLINDER10_ODS	=	12;	//Cylinder10
-const unsigned int GRP_CYLINDER11_ODS	=	13;	//Cylinder11
-const unsigned int GRP_CYLINDER15_ODS	=	14;	//Cylinder15
-const unsigned int GRP_CYLINDER16_ODS	=	15;	//Cylinder16
-const unsigned int GRP_CYLINDER18_ODS	=	16;	//Cylinder18
-const unsigned int GRP_ODS_VESTIBULE_ODS	=	17;	//ODS_vestibule
-const unsigned int GRP_DOCKING_RING_ODS	=	18;	//docking_ring
-const unsigned int GRP_HARDLATCH_ODS	=	19;	//hardlatch
-const unsigned int GRP_SPIRAL_SLEEVES_ODS	=	20;	//spiral_sleeves
-const unsigned int GRP_3SPIRAL_3_ODS	=	21;	//3spiral_3
-const unsigned int GRP_3SPIRAL_2_ODS	=	22;	//3spiral_2
-const unsigned int GRP_3SPIRAL_1_ODS	=	23;	//3spiral_1
-const unsigned int GRP_1L_DRING_EXTENDBASE_ODS	=	24;	//1L_D-ring_extendbase
-const unsigned int GRP_1R_DRING_EXTENDBASE_ODS	=	25;	//1R_D-ring_extendbase
-const unsigned int GRP_1L_DR_ROD_ODS	=	26;	//1L_DR_rod
-const unsigned int GRP_1R_DR_ROD_ODS	=	27;	//1R_DR_rod
-const unsigned int GRP_2L_DRING_EXTENDBASE_ODS	=	28;	//2L_D-ring_extendbase
-const unsigned int GRP_2R_DRING_EXTENDBASE_ODS	=	29;	//2R_D-ring_extendbase
-const unsigned int GRP_2L_DR_ROD_ODS	=	30;	//2L_DR_rod
-const unsigned int GRP_2R_DR_ROD_ODS	=	31;	//2R_DR_rod
-const unsigned int GRP_3L_DRING_EXTENDBASE_ODS	=	32;	//3L_D-ring_extendbase
-const unsigned int GRP_3R_DRING_EXTENDBASE_ODS	=	33;	//3R_D-ring_extendbase
-const unsigned int GRP_3L_DR_ROD_ODS	=	34;	//3L_DR_rod
-const unsigned int GRP_3R_DR_ROD_ODS	=	35;	//3R_DR_rod
+const unsigned int GRP_3R_DR_ROD_ODS	=	0;	//3R_DR_rod
+const unsigned int GRP_3L_DR_ROD_ODS	=	1;	//3L_DR_rod
+const unsigned int GRP_1R_DR_ROD_ODS	=	2;	//1R_DR_rod
+const unsigned int GRP_1L_DR_ROD_ODS	=	3;	//1L_DR_rod
+const unsigned int GRP_3L_DR_RODA_ODS	=	4;	//3L_DR_rod
+const unsigned int GRP_TUBE01_ODS	=	5;	//Tube01
+const unsigned int GRP_BOX65_ODS	=	6;	//Box65
+const unsigned int GRP_TUBE02_ODS	=	7;	//Tube02
+const unsigned int GRP_TUBE04_ODS	=	8;	//Tube04
+const unsigned int GRP_CYLINDER12_ODS	=	9;	//Cylinder12
+const unsigned int GRP_CYLINDER13_ODS	=	10;	//Cylinder13
+const unsigned int GRP_CYLINDER17_ODS	=	11;	//Cylinder17
+const unsigned int GRP_BOX21_ODS	=	12;	//Box21
+const unsigned int GRP_BOX15_ODS	=	13;	//Box15
+const unsigned int GRP_BOX14_ODS	=	14;	//Box14
+const unsigned int GRP_BOX69_ODS	=	15;	//Box69
+const unsigned int GRP_BOX70_ODS	=	16;	//Box70
+const unsigned int GRP_CYLINDER10_ODS	=	17;	//Cylinder10
+const unsigned int GRP_CYLINDER11_ODS	=	18;	//Cylinder11
+const unsigned int GRP_CYLINDER15_ODS	=	19;	//Cylinder15
+const unsigned int GRP_CYLINDER16_ODS	=	20;	//Cylinder16
+const unsigned int GRP_CYLINDER18_ODS	=	21;	//Cylinder18
+const unsigned int GRP_ODS_VESTIBULE_ODS	=	22;	//ODS_vestibule
+const unsigned int GRP_DOCKING_RING_ODS	=	23;	//docking_ring
+const unsigned int GRP_HARDLATCH_ODS	=	24;	//hardlatch
+const unsigned int GRP_SPIRAL_SLEEVES_ODS	=	25;	//spiral_sleeves
+const unsigned int GRP_3SPIRAL_3_ODS	=	26;	//3spiral_3
+const unsigned int GRP_3SPIRAL_2_ODS	=	27;	//3spiral_2
+const unsigned int GRP_3SPIRAL_1_ODS	=	28;	//3spiral_1
+const unsigned int GRP_1L_DRING_EXTENDBASE_ODS	=	29;	//1L_D-ring_extendbase
+const unsigned int GRP_1R_DRING_EXTENDBASE_ODS	=	30;	//1R_D-ring_extendbase
+const unsigned int GRP_2L_DRING_EXTENDBASE_ODS	=	31;	//2L_D-ring_extendbase
+const unsigned int GRP_2R_DRING_EXTENDBASE_ODS	=	32;	//2R_D-ring_extendbase
+const unsigned int GRP_3L_DRING_EXTENDBASE_ODS	=	33;	//3L_D-ring_extendbase
+const unsigned int GRP_3R_DRING_EXTENDBASE_ODS	=	34;	//3R_D-ring_extendbase
+const unsigned int GRP_2R_DR_ROD_ODS	=	35;	//2R_DR_rod
+const unsigned int GRP_2L_DR_ROD_ODS	=	36;	//2L_DR_rod
 
