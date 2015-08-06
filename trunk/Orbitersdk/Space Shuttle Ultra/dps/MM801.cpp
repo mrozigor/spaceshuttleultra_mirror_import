d0016f12c36f6115c899c1c971f93d48793a9485
gls_ssu <gls_ssu@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2015-08-06 22:58:51 +0000

    fixed small omission in MM801 that caused DISPs and SPECs not to show

--- trunk/Orbitersdk/Space Shuttle Ultra/dps/MM801.cpp
+++ trunk/Orbitersdk/Space Shuttle Ultra/dps/MM801.cpp
@@ -1,4 +1,5 @@
 #include "MM801.h"
+#include "IDP.h"
 #include "../Atlantis.h"
 #include <UltraMath.h>
 
@@ -42,6 +43,8 @@ namespace dps
 
 	bool MM801::OnPaint(int spec, vc::MDU* pMDU) const
 	{
+		if (spec != dps::MODE_UNDEFINED) return false;
+
 		PrintCommonHeader(" FCS/DED DIS C/O",pMDU);
 
 		//DED DIS SECTION
