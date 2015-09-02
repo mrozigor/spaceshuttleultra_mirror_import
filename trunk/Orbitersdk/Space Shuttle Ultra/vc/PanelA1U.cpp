9a94d12474496b86b5c2e7a009bec2eda93f752f
gls_ssu <gls_ssu@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2015-09-02 00:49:23 +0000

    corrected click area issues on panel A1U

--- trunk/Orbitersdk/Space Shuttle Ultra/vc/PanelA1U.cpp
+++ trunk/Orbitersdk/Space Shuttle Ultra/vc/PanelA1U.cpp
@@ -8,14 +8,14 @@ namespace vc
 {
 	PanelA1U::PanelA1U(Atlantis* _sts):AtlantisPanel(_sts, "A1U")
 	{
-		Add(pSignalStrengthSelect = new StdSwitch3(_sts, "SIG STR"));
+		/*Add(pSignalStrengthSelect = new StdSwitch3(_sts, "SIG STR"));
 		pSignalStrengthSelect->SetLabel(0, "SBAND FM");
 		pSignalStrengthSelect->SetLabel(1, "KU");
-		pSignalStrengthSelect->SetLabel(2, "SBAND PM");
+		pSignalStrengthSelect->SetLabel(2, "SBAND PM");*/
 
 		Add( pSlewAzimuth = new StdSwitch3( _sts, "SLEW AZIMUTH" ) );
-		pSlewAzimuth->SetLabel( 0, "L" );
-		pSlewAzimuth->SetLabel( 2, "R" );
+		pSlewAzimuth->SetLabel( 0, "R" );
+		pSlewAzimuth->SetLabel( 2, "L" );
 
 		Add( pSlewElevation = new StdSwitch3( _sts, "SLEW EL" ) );
 		pSlewElevation->SetLabel( 0, "DOWN" );
@@ -62,60 +62,58 @@ namespace vc
 	void PanelA1U::DefineVC()
 	{
 		const VECTOR3 switch_rot_vert =  _V( -1, 0, 0 );
-		const VECTOR3 switch_rot_horz = _V( 0.0, -0.9499, 0.3126 );
-		const VECTOR3 rotary_switch_rot = _V( 0.0, -0.3126, -0.9499 );
-		//VECTOR3 switch_pull = _V( 0, 0.3126, 0.9499 );
+		const VECTOR3 switch_rot_horz = _V( 0.0, 1.0, 0.0 );
+		const VECTOR3 rotary_switch_rot = _V( 0.0, 0.0, -1.0 );
 
-		AddAIDToMouseEventList( AID_A1U1 );
-		//AddAIDToMouseEventList( AID_A1U2 );
+		AddAIDToMouseEventList( AID_A1U );
 
 		pSlewAzimuth->SetInitialAnimState( 0.5f );
 		pSlewAzimuth->DefineSwitchGroup( GRP_A1US2_VC );
-		pSlewAzimuth->SetMouseRegion( 0.170449f, 0.618925f, 0.226497f, 0.651137f );//////
+		pSlewAzimuth->SetMouseRegion( 0.378565f, 0.285422f, 0.457113f, 0.370123f );
 		pSlewAzimuth->SetReference( _V( 1.1198, 2.9729, 12.2231 ), switch_rot_horz );
-		pSlewAzimuth->SetOrientation( false );
+		pSlewAzimuth->SetOrientation( true );
 		pSlewAzimuth->SetSpringLoaded( true, 0 );
 		pSlewAzimuth->SetSpringLoaded( true, 2 );
 
 		pSlewElevation->SetInitialAnimState( 0.5f );
 		pSlewElevation->DefineSwitchGroup( GRP_A1US3_VC );
-		pSlewElevation->SetMouseRegion( 0.170449f, 0.618925f, 0.226497f, 0.651137f );//////
+		pSlewElevation->SetMouseRegion( 0.505451f, 0.272174f, 0.574339f, 0.377064f );
 		pSlewElevation->SetReference( _V( 1.0690, 2.9781, 12.2231 ), switch_rot_vert );
 		pSlewElevation->SetSpringLoaded( true, 0 );
 		pSlewElevation->SetSpringLoaded( true, 2 );
 
 		pSlewRate->SetInitialAnimState( 0.5f );
 		pSlewRate->DefineSwitchGroup( GRP_A1US4_VC );
-		pSlewRate->SetMouseRegion( 0.170449f, 0.618925f, 0.226497f, 0.651137f );//////
+		pSlewRate->SetMouseRegion( 0.592866f, 0.272507f, 0.656497f, 0.376257f );
 		pSlewRate->SetReference( _V( 1.0690, 2.9781, 12.2231 ), switch_rot_vert );
 
-		pSteeringMode->SetInitialAnimState( 0.0f );
+		pSteeringMode->SetInitialAnimState( 1.0f );
 		pSteeringMode->DefineSwitchGroup( GRP_A1URS7_VC );
-		pSteeringMode->SetMouseRegion( 0.45049f, 0.440562f, 0.531691f, 0.494226f );/////
+		pSteeringMode->SetMouseRegion( 0.165013f, 0.627728f, 0.267503f, 0.750766f );
 		pSteeringMode->SetReference( _V( 1.1953, 2.8678, 12.2315 ), rotary_switch_rot );
 		pSteeringMode->DefineRotationAngle( 90.0f );
-		pSteeringMode->SetOffset( 45.0f );
+		pSteeringMode->SetOffset( 135.0f );
 		pSteeringMode->SetInitialPosition( 0 );
 
 		pSearch->SetInitialAnimState( 0.5f );
 		pSearch->DefineSwitchGroup( GRP_A1US8_VC );
-		pSearch->SetMouseRegion( 0.170449f, 0.618925f, 0.226497f, 0.651137f );//////
+		pSearch->SetMouseRegion( 0.351805f, 0.644865f, 0.415273f, 0.748752f );
 		pSearch->SetReference( _V( 1.1282, 2.8656, 12.2231 ), switch_rot_vert );
 		pSearch->SetSpringLoaded( true, 1 );
 
 		pPower->SetInitialAnimState( 0.5f );
 		pPower->DefineSwitchGroup( GRP_A1US12_VC );
-		pPower->SetMouseRegion( 0.170449f, 0.618925f, 0.226497f, 0.651137f );//////
+		pPower->SetMouseRegion( 0.030196f, 0.855995f, 0.092852f, 0.958587f );
 		pPower->SetReference( _V( 1.2504, 2.8016, 12.2231 ), switch_rot_vert );
 
 		pMode->SetInitialAnimState( 0.5f );
 		pMode->DefineSwitchGroup( GRP_A1US13_VC );
-		pMode->SetMouseRegion( 0.170449f, 0.618925f, 0.226497f, 0.651137f );//////
+		pMode->SetMouseRegion( 0.162602f, 0.855104f, 0.224663f, 0.961070f );
 		pMode->SetReference( _V( 1.2504, 2.8016, 12.2231 ), switch_rot_vert );
 
 		pRadarOutput->SetInitialAnimState( 0.5f );
 		pRadarOutput->DefineSwitchGroup( GRP_A1US14_VC );
-		pRadarOutput->SetMouseRegion( 0.170449f, 0.618925f, 0.226497f, 0.651137f );//////
+		pRadarOutput->SetMouseRegion( 0.353588f, 0.856309f, 0.416909f, 0.962201f );
 		pRadarOutput->SetReference( _V( 1.2504, 2.8016, 12.2231 ), switch_rot_vert );
 	}
 
@@ -125,19 +123,10 @@ namespace vc
 
 		VECTOR3 ofs = STS()->GetOrbiterCoGOffset() + VC_OFFSET;
 
-		oapiVCRegisterArea( AID_A1U1, PANEL_REDRAW_NEVER, PANEL_MOUSE_LBDOWN | PANEL_MOUSE_LBUP | PANEL_MOUSE_LBPRESSED );
-		oapiVCSetAreaClickmode_Quadrilateral( AID_A1U1, 
-			_V( 1.0151, 3.1734, 12.2217 ) + ofs, _V( 0.8005, 2.9113, 12.2217 ) + ofs, 
-			_V( 1.4696, 2.7409, 12.2217 ) + ofs, _V( 0.8007, 2.7409, 12.2217 ) + ofs );
-		/*oapiVCRegisterArea(AID_A1U1, PANEL_REDRAW_NEVER, PANEL_MOUSE_LBDOWN | PANEL_MOUSE_LBUP | PANEL_MOUSE_LBPRESSED);
-		oapiVCSetAreaClickmode_Quadrilateral(AID_A1U1,
-			_V(-0.264, 2.893, 12.277)+ofs, _V(0.255, 2.893, 12.277)+ofs,
-			_V(-0.264, 2.507, 12.397)+ofs, _V(0.255, 2.507, 12.397)+ofs);
-
-		oapiVCRegisterArea(AID_A1U2, PANEL_REDRAW_NEVER, PANEL_MOUSE_LBDOWN | PANEL_MOUSE_LBUP | PANEL_MOUSE_LBPRESSED);
-		oapiVCSetAreaClickmode_Quadrilateral(AID_A1U2,
-			_V(-0.264, 2.893, 12.277)+ofs, _V(0.255, 2.893, 12.277)+ofs,
-			_V(-0.264, 2.507, 12.397)+ofs, _V(0.255, 2.507, 12.397)+ofs);*/
+		oapiVCRegisterArea( AID_A1U, PANEL_REDRAW_NEVER, PANEL_MOUSE_LBDOWN | PANEL_MOUSE_LBUP | PANEL_MOUSE_LBPRESSED );
+		oapiVCSetAreaClickmode_Quadrilateral( AID_A1U, 
+			_V( 1.2784, 3.0780, 12.2217 ) + ofs, _V( 0.8986, 3.0780, 12.2217 ) + ofs, 
+			_V( 1.2784, 2.7741, 12.2217 ) + ofs, _V( 0.8986, 2.7741, 12.2217 ) + ofs );
 	}
 
 	void PanelA1U::Realize()
