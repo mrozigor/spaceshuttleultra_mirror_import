e097e62f7f1335ffc142d1eae05300851e0373bc
urwumpe <urwumpe@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2016-04-23 18:51:44 +0000

    Fixes for VC2013

--- trunk/Orbitersdk/SSUWorkbench/Orbiter.cs
+++ trunk/Orbitersdk/SSUWorkbench/Orbiter.cs
@@ -5,14 +5,35 @@ using System.Text;
 
 namespace SSUWorkbench.model
 {
+    /// <summary>
+    /// Description of an orbiter resource, that is taking part in a program or mission
+    /// </summary>
+    /// <remarks>
+    /// Describes the properties of an Orbiter that is available in a program/storyline. An 
+    /// orbiter can be set to enter service or be removed from service at a specific date.
+    /// </remarks>
     class Orbiter
     {
+        /// <summary>
+        /// The registration (OV-) or similar of the orbiter
+        /// </summary>
         private string registrationNumber;
+        /// <summary>
+        /// The given name/call sign of the orbiter
+        /// </summary>
         private string name;
-
+        /// <summary>
+        /// The date at which the orbiter became available for a launch campaign
+        /// </summary>
         private DateTime availableDate;
+        /// <summary>
+        /// The date at which the orbiter is taken out of service
+        /// </summary>
+        /// Can be null, for signaling that this orbiter stays active until the end of the program.
         private DateTime outOfServiceDate;
-
+        /// <summary>
+        /// List of missions in the program, which are refering to this orbiter.
+        /// </summary>
         private List<Mission> missions;
     }
 }
