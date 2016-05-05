44d3835913665c0a9b52603f9c5769311ed27352
gls_ssu <gls_ssu@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2016-05-05 02:15:41 +0000

    >> removed unused and non-existing files from projects (vs2010 only) >> removed unused files and projects files

--- trunk/Orbitersdk/Space Shuttle Ultra/resource.h
+++ trunk/Orbitersdk/Space Shuttle Ultra/resource.h
@@ -2,12 +2,6 @@
 // Microsoft Visual C++ generated include file.
 // Used by Atlantis.rc
 //
-#define IDI_UP                          105
-#define IDI_DOWN                        106
-#define IDI_LEFT                        107
-#define IDI_RIGHT                       108
-#define IDI_RRIGHT                      109
-#define IDI_RLEFT                       110
 #define IDB_TKBKLABEL                   113
 #define IDB_CLOCKDIGITS                 115
 #define IDB_7SEGDIGITS                  116
@@ -17,22 +11,7 @@
 #define IDB_SSMELIGHTS                  121
 #define IDB_DEUCHARACTERSOVERBRIGHT     122
 #define IDB_A8LIGHTS                    123
-#define IDC_BUTTON1                     1000
-#define IDC_BUTTON2                     1001
-#define IDC_BUTTON5                     1004
-#define IDC_BUTTON6                     1005
-#define IDC_DEG01                       1006
-#define IDC_PAYLOAD                     1011
-#define IDC_GRAPPLE2                    1011
-#define IDC_BUTTON9                     1024
-#define IDC_BUTTON10                    1025
-#define IDC_BUTTON14                    1029
-#define IDC_BUTTON15                    1030
-#define IDC_BUTTON16                    1031
-#define IDC_BUTTON17                    1032
-#define IDC_DEG1                        1035
-#define IDC_DEG10                       1036
-#define IDC_COMBO1                      1066
+
 
 // Next default values for new objects
 // 
