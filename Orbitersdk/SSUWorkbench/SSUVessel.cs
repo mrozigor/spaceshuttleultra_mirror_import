267217be23dd36ff64792725f6d93827863edf94
gls_ssu <gls_ssu@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2017-01-26 19:46:04 +0000

    >> added code to allow all caps "Context" scenario entry >> fixed bug when saving scenario camera block >> added missing "Brigderails" and "PayloadZPos" mission file parameters >> moved mission file read/write code to mission class >> added launch site, pad, MLP, ET and SRM parameters to mission file >> corrected data binding on some controls >> changes to allow vessels without class name >> added support for "Date JD", "GROUNDLOCATION" and "GROUNDDIRECTION" scenario parameters

--- Orbitersdk/SSUWorkbench/SSUVessel.cs
+++ Orbitersdk/SSUWorkbench/SSUVessel.cs
@@ -117,8 +117,7 @@ namespace SSUWorkbench.model
 				}
 			} while ((line = scn.ReadLine()) != null);
 
-			// TODO check if mission file exists (what to do if it doesn't?)
-			LoadMissionFile( osfspath + "Missions\\SSU\\" + missionname + ".cfg" );
+			mission.Load( osfspath + "Missions\\SSU\\" + missionname + ".cfg" );
 			return;
 		}
 
@@ -150,169 +149,10 @@ namespace SSUWorkbench.model
 			// TODO subsystems
 			// TODO panels
 
-			SaveMissionFile( osfspath + "Missions\\SSU\\" + missionname + ".cfg" );
+			mission.Save( osfspath + "Missions\\SSU\\" + missionname + ".cfg" );
 			return;
 		}
 
-		private void LoadMissionFile( string missionfile )
-		{
-			string line;
-			string key;
-			string val;
-			int eq = -1;
-
-			System.IO.StreamReader file = new System.IO.StreamReader( missionfile );
-			while ((line = file.ReadLine()) != null)
-			{
-				eq = line.IndexOf( "=" );
-
-				if (eq != -1)
-				{
-					key = line.Substring( 0, eq );
-					val = line.Substring( eq + 1, line.Length - eq - 1 );
-					switch (key)
-					{
-						case "Name":
-							mission.MissionName = val;
-							break;
-						case "Orbiter":
-							mission.OVname = val;
-							break;
-						case "OrbiterTexture":
-							mission.OVtex = val;
-							break;
-						case "TargetInc":
-							mission.MECO_Inc = Convert.ToDouble( val );
-							break;
-						case "TargetLAN":
-							mission.MECO_LAN = Convert.ToDouble( val );
-							break;
-						case "MECOAlt":
-							mission.MECO_Alt = Convert.ToDouble( val );
-							break;
-						case "MECOVel":
-							mission.MECO_Vel = Convert.ToDouble( val );
-							break;
-						case "MECOFPA":
-							mission.MECO_FPA = Convert.ToDouble( val );
-							break;
-						case "PerformRollToHeadsUp":
-							mission.RTHU = (val.ToUpperInvariant() == "TRUE" ? true : false);
-							break;
-						case "OMSAssistEnable":
-							mission.OMSAssistEnable = (val.ToUpperInvariant() == "TRUE" ? true : false);
-							break;
-						case "OMSAssistDuration":
-							mission.OMSAssistDuration = Convert.ToDouble( val );
-							break;
-						case "MaxSSMEThrust":
-							mission.MaxThrust = Convert.ToDouble( val );
-							break;
-						case "ThrottleDown":
-							mission.QPOLY3 = Convert.ToDouble( val );
-							break;
-						case "ThrottleUp":
-							mission.QPOLY4 = Convert.ToDouble( val );
-							break;
-						case "UseRMS":
-							mission.RMS = (val.ToUpperInvariant() == "TRUE" ? true : false);
-							break;
-						case "UseKUBand":
-							mission.KUbandAntenna = (val.ToUpperInvariant() == "TRUE" ? true : false);
-							break;
-						case "UseSTBDMPM":
-							mission.STBDMPMs = (val.ToUpperInvariant() == "TRUE" ? true : false);
-							break;
-						case "UseODS":
-							mission.ODS = (val.ToUpperInvariant() == "TRUE" ? true : false);
-							break;
-						case "UseExtAL":
-							mission.ExtAL = (val.ToUpperInvariant() == "TRUE" ? true : false);
-							break;
-						case "UseTAA":
-							mission.TTA = (val.ToUpperInvariant() == "TRUE" ? true : false);
-							break;
-						case "AftTAA":
-							mission.AftTTA = (val.ToUpperInvariant() == "TRUE" ? true : false);
-							break;
-						case "HasBulkheadFloodlights":
-							mission.FwdBulkDockLights = (val.ToUpperInvariant() == "TRUE" ? true : false);
-							break;
-						case "HasDragChute":
-							mission.DragChute = (val.ToUpperInvariant() == "TRUE" ? true : false);
-							break;
-						case "Bridgerails":
-							// TODO
-							break;
-						case "PayloadZPos":
-							// TODO
-							break;
-						case "SILTS":
-							mission.SILTS = (val.ToUpperInvariant() == "TRUE" ? true : false);
-							break;
-						case "LogSSMEData":
-							mission.LogSSMEData = (val.ToUpperInvariant() == "TRUE" ? true : false);
-							break;
-						case "UseCISS":
-							mission.CISS = (val.ToUpperInvariant() == "TRUE" ? true : false);
-							break;
-						case "CISS_GPrime":
-							mission.CISSGPrime = (val.ToUpperInvariant() == "TRUE" ? true : false);
-							break;
-						case "UseASE_IUS":
-							mission.ASE_IUS = (val.ToUpperInvariant() == "TRUE" ? true : false);
-							break;
-						case "ASE_IUS_AftLocation":
-							mission.ASE_IUSAft = (val.ToUpperInvariant() == "TRUE" ? true : false);
-							break;
-
-					}
-				}
-			}
-			file.Close();
-
-			return;
-		}
-
-		private void SaveMissionFile( string missionfile )
-		{
-			System.IO.StreamWriter file = new System.IO.StreamWriter( missionfile );
-			
-			file.WriteLine( "Name=" + mission.MissionName );
-			file.WriteLine( "Orbiter=" + mission.OVname );
-			file.WriteLine( "OrbiterTexture=" + mission.OVtex );
-			file.WriteLine( "TargetInc=" + string.Format( "{0:f6}", mission.MECO_Inc ) );
-			file.WriteLine( "TargetLAN=" + string.Format( "{0:f6}", mission.MECO_LAN ) );
-			file.WriteLine( "MECOAlt=" + string.Format( "{0:f6}", mission.MECO_Alt ) );
-			file.WriteLine( "MECOVel=" + string.Format( "{0:f6}", mission.MECO_Vel ) );
-			file.WriteLine( "MECOFPA=" + string.Format( "{0:f6}", mission.MECO_FPA ) );
-			file.WriteLine( "PerformRollToHeadsUp=" + (mission.RTHU ? "TRUE" : "FALSE") );
-			file.WriteLine( "OMSAssistEnable=" + (mission.OMSAssistEnable ? "TRUE" : "FALSE") );
-			file.WriteLine( "OMSAssistDuration=" + string.Format( "{0:f6}", mission.OMSAssistDuration ) );
-			file.WriteLine( "MaxSSMEThrust=" + mission.MaxThrust );
-			file.WriteLine( "ThrottleDown=" + string.Format( "{0:f3}", mission.QPOLY3 ) );
-			file.WriteLine( "ThrottleUp=" + string.Format( "{0:f3}", mission.QPOLY4 ) );
-			file.WriteLine( "UseRMS=" + (mission.RMS ? "TRUE" : "FALSE") );
-			file.WriteLine( "UseKUBand=" + (mission.KUbandAntenna ? "TRUE" : "FALSE") );
-			file.WriteLine( "UseSTBDMPM=" + (mission.STBDMPMs ? "TRUE" : "FALSE") );
-			file.WriteLine( "UseODS=" + (mission.ODS ? "TRUE" : "FALSE") );
-			file.WriteLine( "UseExtAL=" + (mission.ExtAL ? "TRUE" : "FALSE") );
-			file.WriteLine( "UseTAA=" + (mission.TTA ? "TRUE" : "FALSE") );
-			file.WriteLine( "AftTAA=" + (mission.AftTTA ? "TRUE" : "FALSE") );
-			file.WriteLine( "HasBulkheadFloodlights=" + (mission.FwdBulkDockLights ? "TRUE" : "FALSE") );
-			file.WriteLine( "HasDragChute=" + (mission.DragChute ? "TRUE" : "FALSE") );
-			file.WriteLine( "Bridgerails=" );// TODO
-			file.WriteLine( "PayloadZPos=" );// TODO
-			file.WriteLine( "SILTS=" + (mission.SILTS ? "TRUE" : "FALSE") );
-			file.WriteLine( "LogSSMEData=" + (mission.LogSSMEData ? "TRUE" : "FALSE") );
-			file.WriteLine( "UseCISS=" + (mission.CISS ? "TRUE" : "FALSE") );
-			file.WriteLine( "CISS_GPrime=" + (mission.CISSGPrime ? "TRUE" : "FALSE") );
-			file.WriteLine( "UseASE_IUS=" + (mission.ASE_IUS ? "TRUE" : "FALSE") );
-			file.WriteLine( "ASE_IUS_AftLocation=" + (mission.ASE_IUSAft ? "TRUE" : "FALSE") );
-			
-			file.Close();
-			return;
-		}
 
 		protected string missionname;
 
