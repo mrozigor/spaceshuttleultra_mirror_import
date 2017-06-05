f31c84949c8db7c2f88343e3a34d26a0b2f7bc26
gls_ssu <gls_ssu@ee9e6bc9-0c0e-4b95-8483-90109bf3a774>
2017-06-05 15:48:13 +0000

    fix for ticket #161

---
+++ trunk/Orbitersdk/Space Shuttle Ultra/eva_docking/IntAirlock.cpp
@@ -0,0 +1,13 @@
+#include "IntAirlock.h"
+
+
+namespace eva_docking
+{
+	IntAirlock::IntAirlock( AtlantisSubsystemDirector* _director, const std::string& _ident ):AtlantisSubsystem( _director, _ident ) 
+	{
+	}
+
+	IntAirlock::~IntAirlock()
+	{
+	}
+};
